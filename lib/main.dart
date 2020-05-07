a965b57c4fd5d6d945c6f57df0f87037480d7cd8
Nils Strelow <nils.strelow@student.hpi.uni-potsdam.de>
2020-05-07 15:37:38 +0200

    Test Continous Development Mirror to Github -> Codemagic trigger -> Publish (website for now)

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Nof1 Initial app',
       theme: ThemeData(
         // This is the theme of your application.
         //
