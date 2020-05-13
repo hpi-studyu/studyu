a5b12da075cab4cbaba06c2205963096a33cccc3
Florian Henschel <florian.henschel@student.hpi.de>
2020-05-13 10:35:11 +0200

    fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,28 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:app/main.dart';
+import 'package:app/util/localization.dart';
+import 'package:app/welcome/welcome.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+Widget setup(Widget child) {
+  return MaterialApp(
+    localizationsDelegates: [
+      Nof1LocalizationsDelegate(testing: true),
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+    ],
+    home: child,
+  );
+}
 
 void main() {
   testWidgets('Counter increments smoke test', (tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(setup(WelcomeScreen()));
+    await tester.pumpAndSettle();
 
-    // Verify that Get started is displayed
-    expect(find.text('Get started'), findsOneWidget);
+    expect(find.text('get_started'), findsOneWidget);
   });
 }
