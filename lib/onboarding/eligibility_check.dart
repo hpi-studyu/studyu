635e4db0697b94cc676321b3eed205eed7e174f2
Nils Strelow <strelow.nils@gmail.com>
2020-05-18 20:41:11 +0200

    Display study name as EligibilityScreen title

--- lib/onboarding/eligibility_check.dart
+++ lib/onboarding/eligibility_check.dart
@@ -1,3 +1,5 @@
+import 'package:Nof1/models/models.dart';
+import 'package:Nof1/util/localization.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:research_package/research_package.dart';
@@ -8,8 +10,9 @@ import '../database/repository.dart';
 
 class EligibilityCheckScreen extends StatefulWidget {
   final MaterialPageRoute route;
+  final Study study;
 
-  EligibilityCheckScreen({Key key, @required this.route}) : super(key: key);
+  EligibilityCheckScreen({Key key, @required this.study, @required this.route}) : super(key: key);
 
   @override
   State<EligibilityCheckScreen> createState() => _EligibilityCheckScreenState();
@@ -20,12 +23,9 @@ class _EligibilityCheckScreenState extends State<EligibilityCheckScreen> {
     final formStepResult = result.getStepResultForIdentifier('onboardingFormStepID');
     Function eq = const ListEquality().equals;
     final resultValues = formStepResult.results.values.map((result) => result.results['answer'][0].value).toList();
-    if (eq(resultValues, [
-      0,
-      1,
-      1
-    ])) {
-      Navigator.replaceRouteBelow(context, anchorRoute: widget.route, newRoute: MaterialPageRoute(builder: (context) => DashboardScreen()));
+    if (eq(resultValues, [0, 1, 1])) {
+      Navigator.replaceRouteBelow(context,
+          anchorRoute: widget.route, newRoute: MaterialPageRoute(builder: (context) => DashboardScreen()));
       //changeStatus(true);
     }
   }
@@ -38,7 +38,7 @@ class _EligibilityCheckScreenState extends State<EligibilityCheckScreen> {
           if (snapshot.connectionState == ConnectionState.done) {
             if (snapshot.hasData) {
               return RPUITask(
-                task: createOnboarding(snapshot.data),
+                task: createOnboarding(context, snapshot.data, widget.study),
                 onSubmit: resultCallback,
               );
             } else {
@@ -52,16 +52,18 @@ class _EligibilityCheckScreenState extends State<EligibilityCheckScreen> {
         });
   }
 
-  RPNavigableOrderedTask createOnboarding(List<Question> questions) {
-    final instructionStep = RPInstructionStep(identifier: 'instructionID', title: 'Welcome', detailText: 'This is detailText', footnote: '(1) Important footnote')..text = 'This survey decides, whether you are eligible for the back pain study.';
+  RPNavigableOrderedTask createOnboarding(BuildContext context, List<Question> questions, Study study) {
+    final instructionStep = RPInstructionStep(
+        identifier: 'instructionID',
+        title: Nof1Localizations.of(context).translate(study.id),
+        detailText: 'This is detailText',
+        footnote: '(1) Important footnote')
+      ..text = 'This survey decides, whether you are eligible for the back pain study.';
 
     final questionSteps = <RPQuestionStep>[];
 
     for (var question in questions) {
-      final choices = [
-        RPChoice.withParams(question.option1, 0),
-        RPChoice.withParams(question.option2, 1)
-      ];
+      final choices = [RPChoice.withParams(question.option1, 0), RPChoice.withParams(question.option2, 1)];
       final answerFormat = RPChoiceAnswerFormat.withParams(ChoiceAnswerStyle.SingleChoice, choices);
       questionSteps.add(RPQuestionStep.withAnswerFormat("question${question.id}", question.question, answerFormat));
     }
@@ -72,11 +74,8 @@ class _EligibilityCheckScreenState extends State<EligibilityCheckScreen> {
       ..title = 'Thank You!'
       ..text = 'Continue for your results.';
 
-    final backPainSurveyTask = RPNavigableOrderedTask('backPainSurveyTaskID', [
-      instructionStep,
-      onboardingFormStep,
-      completionStep
-    ]);
+    final backPainSurveyTask =
+        RPNavigableOrderedTask('backPainSurveyTaskID', [instructionStep, onboardingFormStep, completionStep]);
 
     return backPainSurveyTask;
   }
