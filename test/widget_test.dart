dc64bc165b799da1fbec6cfd0229f67fc544022c
Nils Strelow <nils.strelow@student.hpi.de>
2020-05-18 16:32:28 +0200

    Fix import path in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,8 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:app/util/localization.dart';
-import 'package:app/welcome/welcome.dart';
+import 'package:Nof1/util/localization.dart';
+import 'package:Nof1/welcome/welcome.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
