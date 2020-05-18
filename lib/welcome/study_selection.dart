2b2053c1d878b89beeb6276de4eb2740c1410be7
Nils Strelow <strelow.nils@gmail.com>
2020-05-18 20:35:19 +0200

    Use ListView for study selection

--- lib/welcome/study_selection.dart
+++ lib/welcome/study_selection.dart
@@ -1,62 +1,72 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
+import '../models/models.dart';
 import '../onboarding/eligibility_check.dart';
 import '../util/localization.dart';
 
 class StudySelectionScreen extends StatelessWidget {
+  final _availableStudies = [Study("tea_vs_coffee"), Study("weed_vs_alcohol"), Study("back_pain")];
+
+  void navigateToEligibilityCheck(BuildContext context) {
+    if (kIsWeb) {
+      Navigator.push(context, MaterialPageRoute(builder: _buildWebCompatScreen));
+    } else {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => EligibilityCheckScreen(
+                    route: ModalRoute.of(context),
+                  )));
+    }
+  }
+
+  Widget _buildWebCompatScreen(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            "Web doesn't support DB yet.",
+            style: Theme.of(context).textTheme.headline3,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          RaisedButton(
+            onPressed: () => Navigator.pushReplacementNamed(context, "/dashboard"),
+            child: Text("Continue"),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () => print("Tea vs. Coffee"),
-              child: Text(Nof1Localizations.of(context).translate("tea_vs_coffee")),
-            ),
-            SizedBox(height: 20),
-            RaisedButton(
-              onPressed: () => print("Weed vs. Alcohol"),
-              child: Text(Nof1Localizations.of(context).translate("weed_vs_alcohol")),
-            ),
-            SizedBox(height: 20),
-            RaisedButton(
-              onPressed: () {
-                kIsWeb
-                    ? Navigator.push(context, MaterialPageRoute(builder: (_context) {
-                        return Center(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Text(
-                                "Web doesn't support DB yet.",
-                                style: Theme.of(context).textTheme.headline3,
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              RaisedButton(
-                                onPressed: () => Navigator.pushReplacementNamed(context, "/dashboard"),
-                                child: Text("Continue"),
-                              )
-                            ],
-                          ),
-                        );
-                      }))
-                    : Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => EligibilityCheckScreen(
-                                  route: ModalRoute.of(context),
-                                )));
-              },
-              child: Text(Nof1Localizations.of(context).translate("back_pain")),
-            )
-          ],
-        ),
+        child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: _availableStudies.length,
+            itemBuilder: (context, index) {
+              return Center(
+                child: ListTile(
+                  onTap: () {
+                    print(Nof1Localizations.of(context).translate(_availableStudies[index].id));
+                    navigateToEligibilityCheck(context);
+                  },
+                  title: Center(child: Text(Nof1Localizations.of(context).translate(_availableStudies[index].id))),
+                  leading: _availableStudies[index].id == "weed_vs_alcohol"
+                      ? Icon(MdiIcons.cannabis)
+                      : Icon(MdiIcons.accountHeart),
+                  trailing: _availableStudies[index].id == "weed_vs_alcohol"
+                      ? Icon(MdiIcons.glassMugVariant)
+                      : Icon(MdiIcons.pill),
+                ),
+              );
+            }),
       ),
     );
   }
