aadeddfecf8390ae16872c3e071bdfb4f53376aa
Nils Strelow <strelow.nils@gmail.com>
2020-05-22 19:32:18 +0200

    Add ParseServer connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 import 'package:provider/provider.dart';
 
 import 'dashboard/dashboard.dart';
@@ -9,10 +10,23 @@ import 'welcome/about.dart';
 import 'welcome/study_selection.dart';
 import 'welcome/welcome.dart';
 
+const keyParseApplicationId = 'nof1';
+const keyParseServerUrl = 'https://nof1.herokuapp.com/parse';
+const keyParseMasterKey = '2d?xz*63GyZ4nDKp#WPKL5?5HLE=dDKm+epsA%Bp';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   var appLanguage = AppLanguage();
   await appLanguage.fetchLocale();
+  await Parse().initialize(keyParseApplicationId, keyParseServerUrl,
+      masterKey: keyParseMasterKey, debug: true, coreStore: await CoreStoreSharedPrefsImp.getInstance());
+  final response = await Parse().healthCheck();
+  if (response.success) {
+    print('Connection to Parse server successful');
+  } else {
+    print("Failed establishing connection to Parse server");
+  }
+
   runApp(MyApp(
     appLanguage: appLanguage,
   ));
