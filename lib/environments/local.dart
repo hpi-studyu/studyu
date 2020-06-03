7c80d791caa851f19a0f7e2d4724654e1aa2fd0f
Nils Strelow <nils.strelow@student.hpi.de>
2020-06-03 17:57:04 +0200

    Fix local address of emulator

--- lib/environments/local.dart
+++ lib/environments/local.dart
@@ -3,7 +3,7 @@ import 'environment.dart';
 
 final Environment localApp = Environment(
   keyParseApplicationId: 'nof1-local',
-  keyParseServerUrl: 'http://localhost:1337/parse',
+  keyParseServerUrl: 'http://10.0.2.2:1337/parse',
   keyParseMasterKey: 'nof1-local',
   debug: true,
   child: MyApp(),
