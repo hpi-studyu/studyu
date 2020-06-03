8f8d72fa660d602e6b44c5e641a816ce434c0c57
Nils Strelow <strelow.nils@gmail.com>
2020-06-03 17:37:54 +0200

    Adapt key to what is set in the parse repo

--- lib/environments/local.dart
+++ lib/environments/local.dart
@@ -4,7 +4,7 @@ import 'environment.dart';
 final Environment localApp = Environment(
   keyParseApplicationId: 'nof1-local',
   keyParseServerUrl: 'http://localhost:1337/parse',
-  keyParseMasterKey: '2d?xz*63GyZ4nDKp#WPKL5?5HLE=dDKm+epsA%Bp',
+  keyParseMasterKey: 'nof1-local',
   debug: true,
   child: MyApp(),
 );
