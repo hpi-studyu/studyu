723c04615641d4f9656776f4d716bb1901d1b803
Ni_Idea <>
2020-06-04 13:00:22 +0200

    WIP

--- lib/questionnaire_widgets/multiple_choice_question_widget.dart
+++ lib/questionnaire_widgets/multiple_choice_question_widget.dart
@@ -1,12 +1,16 @@
-import 'package:research_package/model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+import '../database/models/questionnaire/answers/answer.dart';
 import '../database/models/questionnaire/questions/multiple_choice_question.dart';
 import 'question_widget.dart';
 
 class MultipleChoiceQuestionWidget extends QuestionWidget {
-  MultipleChoiceQuestionWidget(String identifier) : super(identifier);
 
-  static RPQuestionStep build(MultipleChoiceQuestion question) {
+  MultipleChoiceQuestionWidget({Key key, @required MultipleChoiceQuestion question, @required Function(Answer) onDone})
+      : super(key: key, onDone: onDone, question: question);
+
+  /*MultipleChoiceQuestionWidget.fromQuestion(MultipleChoiceQuestion question) {
     final choices = <RPChoice>[];
     for (var choice in question.choices) {
       final choiceStep = RPChoice.withParams(choice.value, choice.id);
@@ -18,6 +22,24 @@ class MultipleChoiceQuestionWidget extends QuestionWidget {
             : ChoiceAnswerStyle.SingleChoice,
         choices);
     return RPQuestionStep.withAnswerFormat('${question.id}', question.question, answerFormat);
+  }*/
+
+  @override
+  State<MultipleChoiceQuestionWidget> createState() => _MultipleChoiceQuestionWidgetState();
+}
+
+class _MultipleChoiceQuestionWidgetState extends State<MultipleChoiceQuestionWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final multipleChoiceQuestion = widget.question as MultipleChoiceQuestion;
+    final choiceWidgets = multipleChoiceQuestion.choices.map<Widget>((choice) => Card(
+      child: Text(choice.value),
+    )).toList();
+    return widget.getCompleteQuestion(
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        children: choiceWidgets,
+      )
+    );
   }
-  
 }
--- lib/questionnaire_widgets/question_widget.dart
+++ lib/questionnaire_widgets/question_widget.dart
@@ -1,20 +1,41 @@
-import 'package:research_package/model.dart';
+import 'package:flutter/material.dart';
 
+import '../database/models/questionnaire/answers/answer.dart';
 import '../database/models/questionnaire/questions/multiple_choice_question.dart';
 import '../database/models/questionnaire/questions/question.dart';
 import 'multiple_choice_question_widget.dart';
 
-class QuestionWidget extends RPQuestionStep {
-  QuestionWidget(String identifier) : super(identifier);
+abstract class QuestionWidget extends StatefulWidget {
 
-  static RPQuestionStep buildFromQuestion(Question question) {
+  final Function(Answer) onDone;
+  final Question question;
+
+  const QuestionWidget({Key key, @required this.onDone, @required this.question}) : super(key: key);
+
+  factory QuestionWidget.generate({Key key, @required Function(Answer) onDone, @required Question question}) {
     switch (question.runtimeType) {
       case MultipleChoiceQuestion:
-        return MultipleChoiceQuestionWidget.build(question as MultipleChoiceQuestion);
+        return MultipleChoiceQuestionWidget(key, question as MultipleChoiceQuestion);
       default:
         print('Question not supported!');
         return null;
     }
+  }
+
+  Widget getCompleteQuestion(Widget questionSpecificBody) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(question.question),
+            SizedBox(height: 5,),
+            questionSpecificBody,
+          ],
+        ),
+      ),
+    );
   }
 
 }
