b039e4f4094e77bc0c6ce51d7e323a90028da00b
Florian Henschel <florian.henschel@student.hpi.de>
2020-06-04 14:21:27 +0200

    WIP added general question widget

--- lib/questionnaire_widgets/multiple_choice_question_widget.dart
+++ lib/questionnaire_widgets/multiple_choice_question_widget.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-import '../database/models/questionnaire/answers/answer.dart';
+import '../database/models/questionnaire/answers/multiple_choice_answer.dart';
 import '../database/models/questionnaire/questions/multiple_choice_question.dart';
-import 'question_widget.dart';
 
-class MultipleChoiceQuestionWidget extends QuestionWidget {
+class MultipleChoiceQuestionWidget extends StatefulWidget {
 
-  MultipleChoiceQuestionWidget({Key key, @required MultipleChoiceQuestion question, @required Function(Answer) onDone})
-      : super(key: key, onDone: onDone, question: question);
+  final MultipleChoiceQuestion question;
+
+  MultipleChoiceQuestionWidget({Key key, @required this.question});
 
   /*MultipleChoiceQuestionWidget.fromQuestion(MultipleChoiceQuestion question) {
     final choices = <RPChoice>[];
@@ -25,21 +26,48 @@ class MultipleChoiceQuestionWidget extends QuestionWidget {
   }*/
 
   @override
-  State<MultipleChoiceQuestionWidget> createState() => _MultipleChoiceQuestionWidgetState();
+  State<MultipleChoiceQuestionWidget> createState() => _MultipleChoiceQuestionWidgetState(question);
+
 }
 
 class _MultipleChoiceQuestionWidgetState extends State<MultipleChoiceQuestionWidget> {
+  List<Choice> selected = [];
+  final MultipleChoiceQuestion multiQuestion;
+
+  _MultipleChoiceQuestionWidgetState(this.multiQuestion);
+
+  void finishAnswering() {
+    widget.onDone(
+      MultipleChoiceAnswer(
+        multiQuestion.id,
+        DateTime.now(),
+        multiQuestion.id,
+        selected.toSet(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final multipleChoiceQuestion = widget.question as MultipleChoiceQuestion;
-    final choiceWidgets = multipleChoiceQuestion.choices.map<Widget>((choice) => Card(
-      child: Text(choice.value),
-    )).toList();
-    return widget.getCompleteQuestion(
-      Column(
+    final choiceWidgets = multiQuestion.choices
+        .map<Widget>((choice) => GestureDetector(
+            onTap: () => null,
+            child: Card(
+              child: ListTile(
+                title: Text(choice.value),
+                trailing: Visibility(
+                  maintainSize: true,
+                  visible: false,
+                  child: Icon(MdiIcons.checkboxMarked),
+                ),
+              ),
+            )))
+        .toList();
+    //TODO translate
+    final selectionComment = 'Select ${multiQuestion.multiple ? 'at least ' : ''}1';
+    return Column(
         mainAxisSize: MainAxisSize.min,
         children: choiceWidgets,
-      )
-    );
+      );
   }
 }
--- lib/questionnaire_widgets/question_widget.dart
+++ lib/questionnaire_widgets/question_widget.dart
@@ -1,41 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../database/models/questionnaire/answers/answer.dart';
 import '../database/models/questionnaire/questions/multiple_choice_question.dart';
 import '../database/models/questionnaire/questions/question.dart';
 import 'multiple_choice_question_widget.dart';
 
-abstract class QuestionWidget extends StatefulWidget {
-
+class QuestionWidget extends StatelessWidget {
   final Function(Answer) onDone;
   final Question question;
 
   const QuestionWidget({Key key, @required this.onDone, @required this.question}) : super(key: key);
 
-  factory QuestionWidget.generate({Key key, @required Function(Answer) onDone, @required Question question}) {
+  Widget getQuestionBody() {
     switch (question.runtimeType) {
       case MultipleChoiceQuestion:
-        return MultipleChoiceQuestionWidget(key, question as MultipleChoiceQuestion);
+        return MultipleChoiceQuestionWidget(
+          question: question as MultipleChoiceQuestion,
+        );
       default:
         print('Question not supported!');
-        return null;
+        return Container();
     }
   }
 
-  Widget getCompleteQuestion(Widget questionSpecificBody) {
+  @override
+  Widget build(BuildContext context) {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(question.question),
-            SizedBox(height: 5,),
-            questionSpecificBody,
-          ],
+        child: Provider<QuestionWidgetModel>(
+          create: (context) => QuestionWidgetModel(),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(question.question),
+              Selector<QuestionWidgetModel, String>(
+                builder: (context, data, child) => Text(
+                  data,
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                selector: (context, model) => model.additionalDescription,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              getQuestionBody(),
+              SizedBox(
+                height: 5,
+              ),
+              Selector<QuestionWidgetModel, Answer>(
+                builder: (context, data, child) => RaisedButton(
+                  onPressed: data != null ? onDone(data) : null,
+                  //TODO translate
+                  child: Text('Next'),
+                ),
+                selector: (context, model) => model.answer,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
 
+class QuestionWidgetModel extends ChangeNotifier {
+  Answer answer;
+
+  String additionalDescription = '';
 }
