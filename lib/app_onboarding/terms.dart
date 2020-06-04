5ead62deb72d92e24cae3fc1005ac98fed8adf21
Nils Strelow <nils.strelow@student.hpi.de>
2020-06-04 22:47:34 +0200

    Auto format

--- lib/app_onboarding/terms.dart
+++ lib/app_onboarding/terms.dart
@@ -5,13 +5,11 @@ import '../routes.dart';
 import '../util/localization.dart';
 
 class TermsScreen extends StatefulWidget {
-
   @override
   _TermsScreenState createState() => _TermsScreenState();
 }
 
 class _TermsScreenState extends State<TermsScreen> {
-
   bool _acceptedTerms = false;
   bool _acceptedPrivacy = false;
   bool _acceptedDisclaimer = false;
@@ -32,32 +30,27 @@ class _TermsScreenState extends State<TermsScreen> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                ...buildSection(
-                    theme,
+                ...buildSection(theme,
                     title: 'Terms',
                     descriptionText: 'take it or leave it',
                     acknowledgmentText: 'I agree to the terms',
-                    onChange: (val) =>  setState(() => _acceptedTerms = val ),
-                    isChecked: _acceptedTerms
-                ),
-                ...buildSection(
-                    theme,
+                    onChange: (val) => setState(() => _acceptedTerms = val),
+                    isChecked: _acceptedTerms),
+                ...buildSection(theme,
                     title: 'Privacy',
                     descriptionText: 'big brother is watching you!',
                     acknowledgmentText: 'I read and understand the privacy statement',
-                    onChange: (val) =>  setState(() => _acceptedPrivacy = val ),
-                    isChecked: _acceptedPrivacy
-                ),
-                ...buildSection(
-                    theme,
+                    onChange: (val) => setState(() => _acceptedPrivacy = val),
+                    isChecked: _acceptedPrivacy),
+                ...buildSection(theme,
                     title: 'Disclaimer',
                     descriptionText: 'we are not liable',
                     acknowledgmentText: 'I read and understand the disclaimer',
-                    onChange: (val) =>  setState(() => _acceptedDisclaimer = val ),
-                    isChecked: _acceptedDisclaimer
-                ),
+                    onChange: (val) => setState(() => _acceptedDisclaimer = val),
+                    isChecked: _acceptedDisclaimer),
                 RaisedButton(
-                  onPressed: userCanContinue() ? () => Navigator.pushReplacementNamed(context, Routes.studySelection) : null,
+                  onPressed:
+                      userCanContinue() ? () => Navigator.pushReplacementNamed(context, Routes.studySelection) : null,
                   child: Text(Nof1Localizations.of(context).translate('get_started')),
                 ),
               ],
@@ -68,15 +61,12 @@ class _TermsScreenState extends State<TermsScreen> {
     );
   }
 
-  List<Widget> buildSection(ThemeData theme, { String title, String descriptionText, String acknowledgmentText, Function onChange, bool isChecked }) {
+  List<Widget> buildSection(ThemeData theme,
+      {String title, String descriptionText, String acknowledgmentText, Function onChange, bool isChecked}) {
     return <Widget>[
       Text(title, style: theme.textTheme.headline3),
       Text(descriptionText),
-      CheckboxListTile(
-        title: Text(acknowledgmentText),
-        value: isChecked,
-        onChanged: onChange
-      ),
+      CheckboxListTile(title: Text(acknowledgmentText), value: isChecked, onChanged: onChange),
       SizedBox(height: 40),
     ];
   }
