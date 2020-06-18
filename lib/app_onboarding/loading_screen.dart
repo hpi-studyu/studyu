ad0bde78f8c8dff93c1b1260ac245d38662b5c0f
Ni_Idea <>
2020-06-18 16:44:16 +0200

    finished automatic redirect to dashboard

--- lib/app_onboarding/loading_screen.dart
+++ lib/app_onboarding/loading_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../database/daos/study_dao.dart';
 import '../database/models/study_instance.dart';
+import '../environments/environment.dart';
 import '../routes.dart';
 import '../study_onboarding/app_state.dart';
 import '../util/localization.dart';
@@ -16,23 +18,44 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
-  void initState() {
-    super.initState();
-    SharedPreferences.getInstance().then((prefs) {
-      final model = context.read<AppModel>()..activeStudy = StudyInstance();
-      final selectedStudyObjectId = prefs.getString(UserUtils.selectedStudyObjectIdKey);
-      if (selectedStudyObjectId == null) {
-        Navigator.pushReplacementNamed(context, Routes.welcome);
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    initData();
+  }
+
+  Future<void> initParse(Environment env) async {
+    if (!Parse().hasParseBeenInitialized()) {
+      await Parse().initialize(env.keyParseApplicationId, env.keyParseServerUrl,
+          masterKey: env.keyParseMasterKey, debug: env.debug, coreStore: await CoreStoreSharedPrefsImp.getInstance());
+      final response = await Parse().healthCheck();
+      if (response.success) {
+        print('Connection to Parse server successful');
+      } else {
+        print('Failed establishing connection to Parse server');
       }
-      StudyDao.getUserStudy(selectedStudyObjectId).then((studyInstance) {
-        if (studyInstance != null) {
-          model.activeStudy = studyInstance;
-          Navigator.pushReplacementNamed(context, Routes.dashboard);
-        } else {
-          Navigator.pushReplacementNamed(context, Routes.welcome);
-        }
-      });
-    });
+    }
+  }
+
+  Future<void> initStudy() async {
+    final model = context.read<AppModel>()..activeStudy = StudyInstance();
+    final prefs = await SharedPreferences.getInstance();
+    final selectedStudyObjectId = prefs.getString(UserUtils.selectedStudyObjectIdKey);
+    print('Selected study: $selectedStudyObjectId');
+    if (selectedStudyObjectId == null) {
+      Navigator.pushReplacementNamed(context, Routes.welcome);
+    }
+    final studyInstance = await StudyDao.getUserStudy(selectedStudyObjectId);
+    if (studyInstance != null) {
+      model.activeStudy = studyInstance;
+      Navigator.pushReplacementNamed(context, Routes.dashboard);
+    } else {
+      Navigator.pushReplacementNamed(context, Routes.welcome);
+    }
+  }
+
+  Future<void> initData() async {
+    await initParse(Environment.of(context));
+    initStudy();
   }
 
   @override
@@ -40,8 +63,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return Scaffold(
       body: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Text('${Nof1Localizations.of(context).translate('loading')}...'),
+            Text(
+              '${Nof1Localizations.of(context).translate('loading')}...',
+              style: Theme.of(context).textTheme.headline4,
+            ),
             CircularProgressIndicator(),
           ],
         ),
