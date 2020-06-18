7de87cde78002d7a0c13dc49f07cdbd8651d9b55
Alexander Zenner <alexander.zenner@student.hpi.de>
2020-06-18 16:21:09 +0200

    add overview of cycles to journey

--- lib/study_onboarding/journey_overview.dart
+++ lib/study_onboarding/journey_overview.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../database/models/study_instance.dart';
 import '../routes.dart';
+import '../study_onboarding/app_state.dart';
 import '../util/localization.dart';
 
 class JourneyOverviewScreen extends StatefulWidget {
@@ -10,6 +13,8 @@ class JourneyOverviewScreen extends StatefulWidget {
 }
 
 class _JourneyOverviewScreen extends State<JourneyOverviewScreen> {
+  StudyInstance study;
+
   Future<void> getConsentAndNavigateToDashboard(BuildContext context) async {
     final consentGiven = await Navigator.pushNamed(context, Routes.consent);
     if (consentGiven != null && consentGiven) {
@@ -20,6 +25,27 @@ class _JourneyOverviewScreen extends State<JourneyOverviewScreen> {
         duration: Duration(seconds: 30),
       ));
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    study = context.read<AppModel>().activeStudy;
+  }
+
+  List<Widget> buildJourney() {
+    final interventions = <Widget>[];
+    for (var i = 0; i < study.interventionOrder.length; i++) {
+      final index = study.interventionOrder[i];
+      if (index == null) {
+        continue;
+      }
+      final milestone = Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(children: [Text(study.interventionSet.interventions[index].name)]));
+      interventions.add(milestone);
+    }
+    return interventions;
   }
 
   @override
@@ -39,9 +65,11 @@ class _JourneyOverviewScreen extends State<JourneyOverviewScreen> {
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     Text(
-                      'todo put journey here',
+                      study.title,
                       style: theme.textTheme.headline5,
                     ),
+                    SizedBox(height: 40),
+                    ...buildJourney(),
                     SizedBox(height: 40),
                     RaisedButton(
                       onPressed: () => getConsentAndNavigateToDashboard(_context),
