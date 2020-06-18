1be19a1dc8945ef26c5c75e7f9d61e690793d7c0
Darius Ruether <ferenc.ruether@student.hpi.de>
2020-06-18 18:16:16 +0200

    Added grid view for consent screen

--- lib/study_onboarding/consent.dart
+++ lib/study_onboarding/consent.dart
@@ -2,12 +2,46 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../util/localization.dart';
-import 'onboarding_progress.dart';
 
 class ConsentScreen extends StatefulWidget {
   @override
   _ConsentScreenState createState() => _ConsentScreenState();
 }
+
+/*
+class ChangeButtonBackground extends StatefulWidget {
+  @override
+  ChangeButtonBackgroundState createState() {
+    return new ChangeButtonBackgroundState();
+  }
+}
+
+class ChangeButtonBackgroundState extends State<ChangeButtonBackground> {
+  List<Color> _colors = [
+    //Get list of colors
+    Colors.red,
+    Colors.blue,
+    Colors.brown,
+    Colors.teal,
+    Colors.purple
+  ];
+  int _currentIndex = 0;
+
+  _onChanged() {
+    //update with a new color when the user taps button
+    int _colorCount = _colors.length;
+    setState(() {
+      if (_currentIndex == _colorCount - 1) {
+        _currentIndex = 0;
+      } else {
+        _currentIndex += 1;
+      }
+    });
+    //setState(() => (_currentIndex == _colorCount - 1) ? _currentIndex = 1 : _currentIndex += 1);
+  }
+}
+
+ */
 
 class _ConsentScreenState extends State<ConsentScreen> {
   @override
@@ -16,12 +50,105 @@ class _ConsentScreenState extends State<ConsentScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(Nof1Localizations.of(context).translate('consent')),
-        bottom: OnboardingProgress(stage: 2, progress: 1),
+        title: const Text('Einverständnis'),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
+      body: PageView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          GridView.count(
+            primary: false,
+            padding: const EdgeInsets.all(20),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 2,
+            children: <Widget>[
+              Card(
+                color: Colors.blue[100],
+                child: InkWell(
+                  splashColor: Colors.orange.withAlpha(100),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: Container(
+                    width: 300,
+                    height: 100,
+                    child: Text('this'),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.blue[200],
+                child: InkWell(
+                  splashColor: Colors.orange.withAlpha(100),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: Container(
+                    width: 300,
+                    height: 100,
+                    child: Text('is'),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.blue[300],
+                child: InkWell(
+                  splashColor: Colors.orange.withAlpha(100),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: Container(
+                    width: 300,
+                    height: 100,
+                    child: Text('for'),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.blue[400],
+                child: InkWell(
+                  splashColor: Colors.orange.withAlpha(100),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: Container(
+                    width: 300,
+                    height: 100,
+                    child: Text('getting'),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.blue[500],
+                child: InkWell(
+                  splashColor: Colors.orange.withAlpha(100),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: Container(
+                    width: 300,
+                    height: 100,
+                    child: Text('your'),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.blue[600],
+                child: InkWell(
+                  splashColor: Colors.orange.withAlpha(100),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: Container(
+                    width: 300,
+                    height: 100,
+                    child: Text('data'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
             padding: const EdgeInsets.all(16),
             child: Column(
               mainAxisSize: MainAxisSize.min,
@@ -32,13 +159,17 @@ class _ConsentScreenState extends State<ConsentScreen> {
                 ),
                 SizedBox(height: 40),
                 RaisedButton(
+                  onPressed: () => Navigator.pop(context, false),
+                  child: Text(Nof1Localizations.of(context).translate('cancel')),
+                ),
+                RaisedButton(
                   onPressed: () => Navigator.pop(context, true),
                   child: Text(Nof1Localizations.of(context).translate('accept')),
                 )
               ],
             ),
           ),
-        ),
+        ],
       ),
     );
   }
