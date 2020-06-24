0ab4932cd946d332a7cee761ccdcd498ced5cb57
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-06-24 12:14:10 +0200

    Undo some code formatting

--- lib/database/models/study_details.dart
+++ lib/database/models/study_details.dart
@@ -17,37 +17,27 @@ class StudyDetails extends ParseObject implements ParseCloneable {
   StudyDetails clone(Map<String, dynamic> map) => StudyDetails.clone()..fromJson(map);
 
   static const keyQuestionnaire = 'questionnaire';
-
   Questionnaire get questionnaire => Questionnaire.fromJson(get<List<dynamic>>(keyQuestionnaire));
-
   set questionnaire(Questionnaire questionnaire) => set<List<dynamic>>(keyQuestionnaire, questionnaire.toJson());
 
   static const keyEligibility = 'eligibilityCriteria';
-
   List<EligibilityCriterion> get eligibility =>
       get<List<dynamic>>(keyEligibility)?.map((e) => EligibilityCriterion.fromJson(e))?.toList() ?? [];
-
   set eligibility(List<EligibilityCriterion> eligibility) =>
       set<List<dynamic>>(keyEligibility, eligibility.map((e) => e.toJson()).toList());
 
   static const keyInterventionSet = 'interventionSet';
-
   InverventionSet get interventionSet => InverventionSet.fromJson(get<Map<String, dynamic>>(keyInterventionSet));
-
   set interventionSet(InverventionSet interventionSet) =>
       set<Map<String, dynamic>>(keyInterventionSet, interventionSet.toJson());
 
   static const keyObservations = 'observations';
-
   List<Observation> get observations =>
       get<List<dynamic>>(keyObservations)?.map((e) => Observation.fromJson(e))?.toList() ?? [];
-
   set observations(List<Observation> observations) =>
       set<List<dynamic>>(keyObservations, observations.map((e) => e.toJson()).toList());
 
   static const keySchedule = 'schedule';
-
   StudySchedule get schedule => StudySchedule.fromJson(get<Map<String, dynamic>>(keySchedule));
-
   set schedule(StudySchedule schedule) => set<Map<String, dynamic>>(keySchedule, schedule.toJson());
 }
