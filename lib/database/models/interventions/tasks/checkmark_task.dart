92fee85889cb906f9834a517aaa859669c929416
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-06-24 11:34:33 +0200

    Extract common supertask of intervention and observation task

--- lib/database/models/interventions/tasks/checkmark_task.dart
+++ lib/database/models/interventions/tasks/checkmark_task.dart
@@ -1,17 +1,18 @@
 import 'package:json_annotation/json_annotation.dart';
 
-import '../../schedule/schedule.dart';
-import '../task.dart';
+import '../../tasks/schedule.dart';
+import '../intervention_task.dart';
 
 part 'checkmark_task.g.dart';
 
 @JsonSerializable()
-class CheckmarkTask extends Task {
+class CheckmarkTask extends InterventionTask {
   static const String taskType = 'checkmark';
 
   CheckmarkTask();
 
   factory CheckmarkTask.fromJson(Map<String, dynamic> json) => _$CheckmarkTaskFromJson(json);
+
   @override
   Map<String, dynamic> toJson() => _$CheckmarkTaskToJson(this);
 }
