bc0e6ab618e8e9dbfb761dc3e9ecb7313a739539
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-06-24 15:21:34 +0200

    Add question models for sliders

--- lib/database/models/questionnaire/question.dart
+++ lib/database/models/questionnaire/question.dart
@@ -1,12 +1,16 @@
+import 'questions/annotated_scale_question.dart';
 import 'questions/boolean_question.dart';
 import 'questions/choice_question.dart';
+import 'questions/visual_analogue_question.dart';
 
 typedef QuestionParser = Question Function(Map<String, dynamic> data);
 
 abstract class Question {
   static Map<String, QuestionParser> questionTypes = {
-    BooleanQuestion.questionType: (data) => BooleanQuestion.fromJson(data),
-    ChoiceQuestion.questionType: (data) => ChoiceQuestion.fromJson(data)
+    AnnotatedScaleQuestion.questionType: (json) => AnnotatedScaleQuestion.fromJson(json),
+    BooleanQuestion.questionType: (json) => BooleanQuestion.fromJson(json),
+    ChoiceQuestion.questionType: (json) => ChoiceQuestion.fromJson(json),
+    VisualAnalogueQuestion.questionType: (json) => VisualAnalogueQuestion.fromJson(json),
   };
   static const String keyType = 'type';
   String type;
---
+++ lib/database/models/questionnaire/questions/annotated_scale_question.dart
@@ -0,0 +1,29 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'slider_question.dart';
+
+part 'annotated_scale_question.g.dart';
+
+@JsonSerializable()
+class AnnotatedScaleQuestion extends SliderQuestion {
+  static const String questionType = 'annotatedScale';
+
+  List<Annotation> annotations;
+
+  AnnotatedScaleQuestion();
+
+  factory AnnotatedScaleQuestion.fromJson(Map<String, dynamic> json) => _$AnnotatedScaleQuestionFromJson(json);
+  @override
+  Map<String, dynamic> toJson() => _$AnnotatedScaleQuestionToJson(this);
+}
+
+@JsonSerializable()
+class Annotation {
+  int value;
+  String annotation;
+
+  Annotation();
+
+  factory Annotation.fromJson(Map<String, dynamic> json) => _$AnnotationFromJson(json);
+  Map<String, dynamic> toJson() => _$AnnotationToJson(this);
+}
