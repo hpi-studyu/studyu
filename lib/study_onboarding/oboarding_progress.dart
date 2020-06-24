bea4ec8b2d6e4257c7f4ff99b04b5668d41af127
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-06-24 09:37:42 +0200

    Extract Onboarding progress bar

---
+++ lib/study_onboarding/oboarding_progress.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class OnboardingProgress extends StatelessWidget implements PreferredSizeWidget {
+  int stage;
+  double progress;
+
+  OnboardingProgress({@required this.stage, @required this.progress, Key key}) : super(key: key);
+
+  double _getProgressForStage(int stage) {
+    if (stage < this.stage) return 1;
+    if (stage == this.stage) return progress;
+    return 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: LinearProgressIndicator(value: _getProgressForStage(0))),
+        SizedBox(width: 4),
+        Expanded(child: LinearProgressIndicator(value: _getProgressForStage(1))),
+        SizedBox(width: 4),
+        Expanded(child: LinearProgressIndicator(value: _getProgressForStage(2))),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(4);
+}
