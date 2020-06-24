60a95867795006c6818848590e8f94e2b0c29cf2
Alexander Zenner <alexander.zenner@student.hpi.de>
2020-06-24 12:19:42 +0200

    create multimap with schedule

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,8 +1,12 @@
+import 'package:Nof1/database/models/interventions/task.dart';
+import 'package:Nof1/database/models/schedule/fixed_schedule.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:quiver/collection.dart';
 
 import '../database/models/interventions/intervention.dart';
+import '../database/models/study_instance.dart';
 import '../routes.dart';
 import '../study_onboarding/app_state.dart';
 import '../util/localization.dart';
@@ -22,6 +26,7 @@ class PlannedIntervention {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  StudyInstance study;
   List<Intervention> interventions;
   List<PlannedIntervention> plannedInterventions;
 
@@ -29,7 +34,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   void initState() {
     super.initState();
     // TODO: dashboard should read from a different model connected to parse UserStudy object
-    final study = context.read<AppModel>().activeStudy;
+    study = context.read<AppModel>().activeStudy;
     interventions = study.interventionSet.interventions.toList();
     plannedInterventions = [];
     for (var i = 0; i < study.interventionOrder.length; i++) {
@@ -44,6 +49,17 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final scheduleToday = Multimap<Time, Task>();
+    study.interventionSet.interventions[0].tasks.forEach((task) {
+      task.schedule.forEach((schedule) {
+        if (schedule is FixedSchedule) {
+          scheduleToday.add(schedule.time, task);
+        }
+      });
+    });
+
+    scheduleToday.forEach((key, value) => print('[$key->$value]'));
+
     return Scaffold(
       appBar: AppBar(
         // Removes back button. We currently keep navigation stack to make developing easier
