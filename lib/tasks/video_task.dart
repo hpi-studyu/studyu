27d31384bbf5cfc267925e3f94f14e9be01ad036
Nils Strelow <strelow.nils@gmail.com>
2020-06-25 17:20:47 +0200

    Add new real tasks to overview

--- lib/tasks/video_task.dart
+++ lib/tasks/video_task.dart
@@ -3,12 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 import '../util/localization.dart';
-import 'dashboard_task.dart';
 
-class VideoTask extends DashboardTask {
+class VideoTask extends StatefulWidget {
   final String _asset;
 
-  VideoTask(title, description, this._asset) : super(title, description, icon: Icon(Icons.ondemand_video));
+  const VideoTask(this._asset);
 
   @override
   State<VideoTask> createState() => _VideoTaskState();
@@ -37,26 +36,20 @@ class _VideoTaskState extends State<VideoTask> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(Nof1Localizations.of(context).translate('video_task')),
-      ),
-      body: Center(
+    return Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Expanded(
-              child: Chewie(
-                controller: _chewieController,
-              ),
-            ),
-            RaisedButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text(Nof1Localizations.of(context).translate('finished')),
-            )
-          ],
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Expanded(
+          child: Chewie(
+            controller: _chewieController,
+          ),
         ),
-      ),
-    );
+        RaisedButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text(Nof1Localizations.of(context).translate('finished')),
+        )
+      ],
+    ));
   }
 }
