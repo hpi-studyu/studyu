68031c1aacea2077083c351aa21241eeafd5b193
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-06-25 08:53:35 +0200

    Add widget for visual analogue question

--- lib/database/models/questionnaire/questions/slider_question.dart
+++ lib/database/models/questionnaire/questions/slider_question.dart
@@ -1,7 +1,10 @@
+import '../answer.dart';
 import '../question.dart';
 
 abstract class SliderQuestion extends Question {
   int minimum;
   int maximum;
   int step;
+
+  Answer<int> constructAnswer(int response) => Answer.forQuestion(this, response);
 }
