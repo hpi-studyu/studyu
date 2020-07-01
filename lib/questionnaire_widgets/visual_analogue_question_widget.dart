2590021d603f001676c548954f747dd6e2269c29
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-07-01 15:56:55 +0200

    Refactor slider members

--- lib/questionnaire_widgets/visual_analogue_question_widget.dart
+++ lib/questionnaire_widgets/visual_analogue_question_widget.dart
@@ -16,17 +16,17 @@ class VisualAnalogueQuestionWidget extends QuestionWidget {
 }
 
 class _VisualAnalogueQuestionWidgetState extends State<VisualAnalogueQuestionWidget> {
-  int value;
+  double value;
 
   @override
   void initState() {
     super.initState();
-    value = widget.question.minimum;
+    value = widget.question.initial;
   }
 
   void changed(double value) {
     setState(() {
-      this.value = value.toInt();
+      this.value = value;
     });
   }
 
@@ -34,15 +34,37 @@ class _VisualAnalogueQuestionWidgetState extends State<VisualAnalogueQuestionWid
   Widget build(BuildContext context) {
     return Column(
       children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+          child: Column(children: [
+            Row(
+              children: [
+                Expanded(child: Text(widget.question.minimumAnnotation)),
+                FittedBox(child: Text(widget.question.maximumAnnotation)),
+              ],
+            ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [widget.question.minimumColor, widget.question.maximumColor],
+                ),
+              ),
+              constraints: BoxConstraints.expand(height: 4),
+            ),
+          ]),
+        ),
         Slider(
-          value: value.toDouble(),
+          value: value,
           onChanged: changed,
-          min: widget.question.minimum.toDouble(),
-          max: widget.question.maximum.toDouble(),
+          min: widget.question.minimum,
+          max: widget.question.maximum,
           divisions: (widget.question.maximum - widget.question.minimum) ~/ widget.question.step,
         ),
         RaisedButton(
           onPressed: () => widget.onDone(widget.question.constructAnswer(value)),
+          child: Text('done'),
         )
       ],
     );
