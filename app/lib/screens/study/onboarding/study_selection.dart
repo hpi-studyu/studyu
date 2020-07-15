fb83f125b2a9e87aeb94055a4e57b1681bcf7a70
Nils Strelow <nils.strelow@student.hpi.de>
2020-07-16 00:35:17 +0200

    Move StudyUtils (StudyDao) to utils

--- app/lib/screens/study/onboarding/study_selection.dart
+++ app/lib/screens/study/onboarding/study_selection.dart
@@ -3,10 +3,10 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 import 'package:provider/provider.dart';
 import 'package:studyou_core/models/models.dart';
 
-import '../../../database/daos/study.dart';
 import '../../../models/app_state.dart';
 import '../../../routes.dart';
 import '../../../util/localization.dart';
+import '../../../util/study.dart';
 
 class StudySelectionScreen extends StatefulWidget {
   @override
