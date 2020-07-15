8a7ef76fcd774c800e5bf69cae67d5d82c1749db
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-07-15 11:15:50 +0200

    Move navigation tasks out of questionnaire_widget

--- lib/study_onboarding/eligibility_screen.dart
+++ lib/study_onboarding/eligibility_screen.dart
@@ -4,8 +4,15 @@ import 'package:nof1_models/models/models.dart';
 import '../questionnaire_widgets/questionnaire_widget.dart';
 import 'onboarding_progress.dart';
 
+class EligibilityResult {
+  final bool conditionResult;
+  final QuestionnaireState answers;
+
+  EligibilityResult(this.answers, {this.conditionResult});
+}
+
 class EligibilityScreen extends StatelessWidget {
-  static MaterialPageRoute<QuestionnaireResult> routeFor(List<Question> questions,
+  static MaterialPageRoute<EligibilityResult> routeFor(List<Question> questions,
           {@required String title, List<EligibilityCriterion> criteria}) =>
       MaterialPageRoute(
           builder: (_) => EligibilityScreen(questions, title: title, criteria: criteria),
@@ -17,6 +24,11 @@ class EligibilityScreen extends StatelessWidget {
 
   const EligibilityScreen(this.questions, {this.title, this.criteria, Key key}) : super(key: key);
 
+  void _evaluateResponse(QuestionnaireState qs, BuildContext context) {
+    final conditionResult = criteria?.every((criterion) => criterion.isSatisfied(qs)) ?? true;
+    Navigator.of(context).pop(EligibilityResult(qs, conditionResult: conditionResult));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +36,7 @@ class EligibilityScreen extends StatelessWidget {
         title: Text(title),
         bottom: OnboardingProgress(stage: 0, progress: 0.5),
       ),
-      body: QuestionnaireWidget(questions, title: title, criteria: criteria),
+      body: QuestionnaireWidget(questions, title: title, onComplete: (qs) => _evaluateResponse(qs, context)),
     );
   }
 }
