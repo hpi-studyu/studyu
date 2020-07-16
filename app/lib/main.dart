724cf340735b473181344b324f29e01a21aba3bf
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-07-16 10:02:40 +0200

    Expected result: only run app

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,6 +5,6 @@ import 'environments/dev.dart';
 // Runs the dev/staging environment. Named main to make it easier to setup
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
+  print('hi');
   runApp(devApp);
 }
