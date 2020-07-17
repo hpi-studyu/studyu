3c5785ed79e3bd4982c3203d4b19278c1dc5a9b3
Nils Strelow <strelow.nils@gmail.com>
2020-07-17 13:14:40 +0200

    Add theme

--- study_designer/lib/app.dart
+++ study_designer/lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:studyou_core/util/parse_init.dart';
 
 import 'dashboard.dart';
+import 'theme.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -11,12 +12,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Study Designer',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: ParseInit(child: Dashboard()));
+    return MaterialApp(title: 'Study Designer', theme: theme, home: ParseInit(child: Dashboard()));
   }
 }
