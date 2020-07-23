e1a6399916a5c70e0a23bcb06b799518d6ac637c
Nils Strelow <strelow.nils@gmail.com>
2020-07-23 14:25:33 +0200

    Remove particles from RoundCheckbox

--- app/lib/widgets/round_checkbox.dart
+++ app/lib/widgets/round_checkbox.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:pimp_my_button/pimp_my_button.dart';
 
 class RoundCheckbox extends StatelessWidget {
   final Function(bool) onChanged;
@@ -11,27 +10,21 @@ class RoundCheckbox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return PimpedButton(
-      particle: DemoParticle(),
-      pimpedWidgetBuilder: (context, controller) {
-        return IconButton(
-          onPressed: () {
-            onChanged(!value);
-            controller.forward(from: 0);
-          },
-          icon: value
-              ? Icon(
-                  MdiIcons.checkboxMarkedCircleOutline,
-                  size: 30,
-                  color: theme.accentColor,
-                )
-              : Icon(
-                  MdiIcons.checkboxBlankCircleOutline,
-                  size: 30,
-                  color: theme.accentColor,
-                ),
-        );
+    return IconButton(
+      onPressed: () {
+        onChanged(!value);
       },
+      icon: value
+          ? Icon(
+              MdiIcons.checkboxMarkedCircleOutline,
+              size: 30,
+              color: theme.accentColor,
+            )
+          : Icon(
+              MdiIcons.checkboxBlankCircleOutline,
+              size: 30,
+              color: theme.accentColor,
+            ),
     );
   }
 }
