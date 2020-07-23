d669c8a69668cee90e2c8da66b45ac5d0c3ae21a
Nils Strelow <nils.strelow@student.hpi.de>
2020-07-23 17:05:46 +0200

    Use BaselineCard in InterventionCard

--- app/lib/screens/study/dashboard/task_overview_tab/progress_row.dart
+++ app/lib/screens/study/dashboard/task_overview_tab/progress_row.dart
@@ -1,10 +1,10 @@
+import 'package:StudYou/screens/study/onboarding/intervention_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:studyou_core/models/models.dart';
 
 import '../../../../util/intervention.dart';
-import '../../onboarding/intervention_card.dart';
 
 class ProgressRow extends StatefulWidget {
   final StudyInstance study;
@@ -73,9 +73,7 @@ class InterventionSegment extends StatelessWidget {
                   context: context,
                   builder: (context) => AlertDialog(
                         contentPadding: EdgeInsets.all(0),
-                        content: isBaseline(intervention)
-                            ? BaselineCard()
-                            : InterventionCard(intervention, showCheckbox: false),
+                        content: InterventionCard(intervention, showCheckbox: false),
                       ));
             },
             elevation: 0,
@@ -84,32 +82,5 @@ class InterventionSegment extends StatelessWidget {
             child: interventionIcon(intervention)),
       ],
     ));
-  }
-}
-
-class BaselineCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        ListTile(
-          leading: Icon(MdiIcons.rayStart),
-          dense: true,
-          title: Text(
-            'Baseline',
-            style: theme.textTheme.headline6,
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(16, 4, 16, 8),
-          child: Text(
-            'A baseline study is an analysis of the current situation to identify the starting points for a programme or project. It looks at what information must be considered and analyzed to establish a baseline or starting point, the benchmark against which future progress can be assessed or comparisons made.',
-            style: theme.textTheme.bodyText2.copyWith(color: theme.textTheme.caption.color),
-          ),
-        )
-      ],
-    );
   }
 }
