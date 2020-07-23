14f18ea4c5f41da09c86131dccb88fe863899481
Nils Strelow <nils.strelow@student.hpi.de>
2020-07-23 17:30:55 +0200

    Intersperse the progress row connector lines

--- app/lib/screens/study/dashboard/task_overview_tab/progress_row.dart
+++ app/lib/screens/study/dashboard/task_overview_tab/progress_row.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intersperse/intersperse.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:studyou_core/models/models.dart';
 
@@ -18,6 +19,16 @@ class _ProgressRowState extends State<ProgressRow> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+
+    final divider = Expanded(
+      child: Divider(
+        indent: 5,
+        endIndent: 5,
+        thickness: 3,
+        color: theme.primaryColor,
+      ),
+    );
+
     return Padding(
       padding: const EdgeInsets.all(8),
       child: Stack(
@@ -28,23 +39,15 @@ class _ProgressRowState extends State<ProgressRow> {
             children: [
               Icon(MdiIcons.run, size: 30),
               SizedBox(width: 8),
-              ...widget.study.getInterventionsInOrder().asMap().entries.expand((entry) {
+              ...intersperse(divider, widget.study.getInterventionsInOrder().asMap().entries.map((entry) {
                 final currentInterventionIndex = widget.study.getInterventionIndexForDate(DateTime.now());
-                return [
+                return
                   InterventionSegment(
                       intervention: entry.value,
                       isCurrent: currentInterventionIndex == entry.key,
-                      isFuture: currentInterventionIndex < entry.key),
-                  Expanded(
-                    child: Divider(
-                      indent: 5,
-                      endIndent: 5,
-                      thickness: 3,
-                      color: theme.primaryColor,
-                    ),
-                  ),
-                ];
-              }),
+                      isFuture: currentInterventionIndex < entry.key);
+              })),
+              SizedBox(width: 8),
               Icon(MdiIcons.flagCheckered, size: 30),
             ],
           ),
