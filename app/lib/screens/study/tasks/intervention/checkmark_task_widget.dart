0d954ad16b84bb6cd9cfac855556ad114dbb525d
Nils Strelow <nils.strelow@student.hpi.de>
2020-07-23 15:22:12 +0200

    Add particles to CheckmarkTask

--- app/lib/screens/study/tasks/intervention/checkmark_task_widget.dart
+++ app/lib/screens/study/tasks/intervention/checkmark_task_widget.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:pimp_my_button/pimp_my_button.dart';
 
 class CheckmarkTaskWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RaisedButton.icon(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        icon: Icon(Icons.check),
-        label: Text('Complete', style: TextStyle(fontSize: 24)));
+    return PimpedButton(
+        particle: DemoParticle(),
+        pimpedWidgetBuilder: (context, controller) => RaisedButton.icon(
+            onPressed: () async {
+              await controller.forward(from: 0);
+              await Future.delayed(Duration(milliseconds: 100));
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.check, size: 32),
+            label: Text('Complete', style: TextStyle(fontSize: 24))));
   }
 }
