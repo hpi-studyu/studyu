8f1414acba90c0b0e84bf84c415fa652108f5cf2
fhenschel <florian.henschel@solutiance.com>
2020-07-23 18:32:07 +0200

    added first version of power level bar

--- app/lib/screens/study/report/report_details.dart
+++ app/lib/screens/study/report/report_details.dart
@@ -2,9 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:rainbow_color/rainbow_color.dart';
 import 'package:studyou_core/models/models.dart';
 
 import '../../../models/app_state.dart';
+import '../../../util/localization.dart';
 
 class ReportDetailsScreen extends StatelessWidget {
   @override
@@ -69,7 +71,98 @@ class ReportPerformanceModule extends ReportModuleContent {
   const ReportPerformanceModule(StudyInstance instance) : super(instance);
 
   @override
-  Widget build(BuildContext context) => Card();
+  Widget build(BuildContext context) => Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: Text(
+                '${Nof1Localizations.of(context).translate('current_power_level')}: ${getPowerLevelDescription()}'),
+          ),
+          PerformanceBar(
+            progress: 0.4,
+            minimum: 0.6,
+          ),
+        ],
+      );
+
+  String getPowerLevelDescription() {
+    // TODO add useful power level wording
+    return 'OVER 9000';
+  }
+}
+
+class PerformanceBar extends StatelessWidget {
+  final double progress;
+  final double minimum;
+
+  const PerformanceBar({@required this.progress, this.minimum, Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final rainbow = Rainbow(spectrum: [Colors.red, Colors.yellow, Colors.green], rangeStart: 0, rangeEnd: 1);
+    final fullSpectrum = List<double>.generate(3, (index) => index * 0.5)
+        .map<Color>((index) => rainbow[index].withOpacity(0.4))
+        .toList();
+    final colorSamples =
+        List<double>.generate(11, (index) => index * 0.1 * progress).map<Color>((index) => rainbow[index]).toList();
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 20,
+          width: double.infinity,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: fullSpectrum,
+                    ),
+                  ),
+                ),
+                FractionallySizedBox(
+                  alignment: Alignment.centerLeft,
+                  widthFactor: progress,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border(right: BorderSide(width: 2, color: Colors.grey[600])),
+                      gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: colorSamples,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        if (minimum != null && minimum >= 0 && minimum <= 1)
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              FractionallySizedBox(
+                widthFactor: minimum,
+                child: Container(
+                  height: 15,
+                  decoration: BoxDecoration(
+                    border: Border(right: BorderSide(width: 2, color: Colors.grey[600])),
+                  ),
+                ),
+              ),
+              Text('min'),
+            ],
+          ),
+      ],
+    );
+  }
 }
 
 class ReportOutcomeModule extends ReportModuleContent {
