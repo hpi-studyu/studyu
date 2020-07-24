f713add98bbba73ad26f3a76aaf7975d135f331f
Darius Ruether <ferenc.ruether@student.hpi.de>
2020-07-24 10:29:22 +0200

    Changed icons and description text

--- app/lib/screens/app_onboarding/about.dart
+++ app/lib/screens/app_onboarding/about.dart
@@ -10,7 +10,7 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(Nof1Localizations.of(context).translate('what_is_nof1')),
+        title: Text(Nof1Localizations.of(context).translate('what_is_studyu')),
       ),
       body: PageView(
         scrollDirection: Axis.vertical,
@@ -19,16 +19,16 @@ class AboutScreen extends StatelessWidget {
             padding: const EdgeInsets.all(16),
             child:
                 Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              SizedBox(height: 100),
+              SizedBox(height: 50),
               Row(children: [
                 Expanded(
-                  child: Icon(MdiIcons.coffee, size: 80, color: Colors.black),
+                  child: Icon(MdiIcons.food, size: 80, color: Colors.black),
                 ),
                 Expanded(
                   child: Icon(MdiIcons.equal, size: 80, color: Colors.black),
                 ),
                 Expanded(
-                  child: Icon(MdiIcons.sleep, size: 80, color: Colors.black),
+                  child: Icon(MdiIcons.sleepOff, size: 80, color: Colors.black),
                 ),
               ]),
               SizedBox(height: 100),
@@ -53,7 +53,7 @@ class AboutScreen extends StatelessWidget {
             padding: const EdgeInsets.all(16),
             child:
                 Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              SizedBox(height: 100),
+              SizedBox(height: 50),
               Row(children: [
                 Expanded(
                   child: Icon(MdiIcons.help, size: 80, color: Colors.orange),
@@ -81,43 +81,15 @@ class AboutScreen extends StatelessWidget {
             padding: const EdgeInsets.all(16),
             child:
                 Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              SizedBox(height: 100),
-              Row(children: [
-                Expanded(
-                  child: Icon(MdiIcons.exclamationThick, size: 80, color: Colors.blue),
-                ),
-              ]),
               SizedBox(height: 50),
-              Text(Nof1Localizations.of(context).translate('description_part3'),
-                  textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
-              Expanded(
-                child: Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: Padding(
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Icon(
-                      Icons.arrow_drop_down,
-                      color: Colors.blue,
-                      size: 60,
-                    ),
-                  ),
-                ),
-              ),
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child:
-                Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              SizedBox(height: 100),
               Row(children: [
                 Expanded(
                   child: Icon(MdiIcons.accountQuestion, size: 80, color: Colors.blue),
                 ),
               ]),
               SizedBox(height: 50),
-              Text(Nof1Localizations.of(context).translate('description_part4'),
-                  textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+              Text(Nof1Localizations.of(context).translate('description_part3'),
+                  textAlign: TextAlign.justify, style: TextStyle(fontSize: 18)),
               Expanded(
                 child: Align(
                   alignment: FractionalOffset.bottomCenter,
@@ -137,15 +109,80 @@ class AboutScreen extends StatelessWidget {
             padding: const EdgeInsets.all(16),
             child:
                 Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              SizedBox(height: 100),
+              SizedBox(height: 50),
+              Row(children: [
+                Expanded(
+                  child: Icon(MdiIcons.exclamationThick, size: 80, color: Colors.blue),
+                )
+              ]),
+              SizedBox(height: 50),
+              Text(Nof1Localizations.of(context).translate('description_part4'),
+                  textAlign: TextAlign.justify, style: TextStyle(fontSize: 18)),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.blue,
+                      size: 60,
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child:
+                Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              SizedBox(height: 50),
+              Row(children: [
+                Expanded(
+                  child: Icon(MdiIcons.alphaNBoxOutline, size: 80, color: Colors.blue),
+                ),
+                Expanded(
+                    child: Text(
+                  "of",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30),
+                )),
+                Expanded(
+                  child: Icon(MdiIcons.numeric1BoxOutline, size: 80, color: Colors.blue),
+                ),
+              ]),
+              SizedBox(height: 50),
+              Text(Nof1Localizations.of(context).translate('description_part5'),
+                  textAlign: TextAlign.justify, style: TextStyle(fontSize: 18)),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.blue,
+                      size: 60,
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child:
+                Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              SizedBox(height: 50),
               Row(children: [
                 Expanded(
                   child: Icon(MdiIcons.notebookOutline, size: 80, color: Colors.blue),
                 ),
               ]),
               SizedBox(height: 50),
-              Text(Nof1Localizations.of(context).translate('description_part5'),
-                  textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+              Text(Nof1Localizations.of(context).translate('description_part6'),
+                  textAlign: TextAlign.justify, style: TextStyle(fontSize: 18)),
               Expanded(
                 child: Align(
                   alignment: FractionalOffset.bottomCenter,
@@ -165,9 +202,77 @@ class AboutScreen extends StatelessWidget {
             padding: const EdgeInsets.all(16),
             child:
                 Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Image(image: AssetImage('assets/images/icon.png'), height: 100),
               SizedBox(height: 50),
-              Text(Nof1Localizations.of(context).translate('description_part6'),
+              Row(children: [
+                Expanded(
+                  child: Icon(MdiIcons.alignVerticalBottom, size: 80, color: Colors.blue),
+                ),
+              ]),
+              SizedBox(height: 50),
+              Text(Nof1Localizations.of(context).translate('description_part7'),
+                  textAlign: TextAlign.justify, style: TextStyle(fontSize: 18)),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.blue,
+                      size: 60,
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child:
+                Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              SizedBox(height: 50),
+              Row(children: [
+                Expanded(
+                  child: Icon(MdiIcons.progressCheck, size: 80, color: Colors.blue),
+                ),
+              ]),
+              SizedBox(height: 50),
+              Text(Nof1Localizations.of(context).translate('description_part8'),
+                  textAlign: TextAlign.justify, style: TextStyle(fontSize: 18)),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.blue,
+                      size: 60,
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child:
+                Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    "Study",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 30),
+                  )),
+                  Expanded(
+                    child: Icon(MdiIcons.stethoscope, size: 80, color: Colors.blue),
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+              Text(Nof1Localizations.of(context).translate('description_part9'),
                   textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
               SizedBox(height: 40),
               RaisedButton(
