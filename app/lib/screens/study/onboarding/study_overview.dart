66044270054f3ad64757d96c45263e87d3b7c8d8
Nils Strelow <nils.strelow@student.hpi.de>
2020-07-28 13:11:58 +0200

    Remove appbars without progress

--- app/lib/screens/study/onboarding/study_overview.dart
+++ app/lib/screens/study/onboarding/study_overview.dart
@@ -8,7 +8,6 @@ import 'package:studyou_core/util/parse_future_builder.dart';
 
 import '../../../models/app_state.dart';
 import '../../../routes.dart';
-import '../../../util/localization.dart';
 import '../../../widgets/bottom_onboarding_navigation.dart';
 import 'eligibility_screen.dart';
 
@@ -48,9 +47,6 @@ class _StudyOverviewScreen extends State<StudyOverviewScreen> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(Nof1Localizations.of(context).translate('summary')),
-      ),
       body: Center(
           child: ParseFetchOneFutureBuilder<Study>(
               queryFunction: () => _futureStudyDetails,
