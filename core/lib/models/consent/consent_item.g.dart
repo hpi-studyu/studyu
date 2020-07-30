057e7df6731592fe958a225596a887322e879c84
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-07-30 15:57:14 +0200

    Add missing file

---
+++ core/lib/models/consent/consent_item.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'consent_item.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ConsentItem _$ConsentItemFromJson(Map<String, dynamic> json) {
+  return ConsentItem()
+    ..id = json['id'] as String
+    ..title = json['title'] as String
+    ..description = json['description'] as String
+    ..iconName = json['iconName'] as String;
+}
+
+Map<String, dynamic> _$ConsentItemToJson(ConsentItem instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'description': instance.description,
+      'iconName': instance.iconName,
+    };
