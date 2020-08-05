157ad6ae28504a1661738c641eaac02210a44513
Ni_Idea <>
2020-08-05 23:19:21 +0200

    fixed deleting wrong choice and 'flutterified' choice list

--- study_designer/lib/widgets/choice_question_edit_widget.dart
+++ study_designer/lib/widgets/choice_question_edit_widget.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:study_designer/widgets/choice_edit_widget.dart';
 import 'package:studyou_core/models/models.dart';
 import 'package:uuid/uuid.dart';
+
+import 'choice_edit_widget.dart';
 
 class ChoiceQuestionEditWidget extends StatefulWidget {
   final ChoiceQuestion question;
@@ -31,12 +32,25 @@ class _ChoiceQuestionEditWidgetState extends State<ChoiceQuestionEditWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
+    /*return Column(children: [
       RaisedButton.icon(onPressed: _addChoice, icon: Icon(Icons.add), color: Colors.green, label: Text('Add Choice')),
-      ...widget.question.choices
-          .asMap()
-          .entries
-          .map((entry) => ChoiceEditWidget(choice: entry.value, remove: () => _removeChoice(entry.key)))
-    ]);
+      ...widget.question.choices.asMap().entries.map(
+          (entry) => ChoiceEditWidget(key: UniqueKey(), choice: entry.value, remove: () => _removeChoice(entry.key)))
+    ]);*/
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: widget.question.choices.length + 1,
+      itemBuilder: (buildContext, index) {
+        return index == widget.question.choices.length
+            ? Row(children: [
+                Spacer(),
+                RaisedButton.icon(
+                    onPressed: _addChoice, icon: Icon(Icons.add), color: Colors.green, label: Text('Add Choice')),
+                Spacer()
+              ])
+            : ChoiceEditWidget(
+                key: UniqueKey(), choice: widget.question.choices[index], remove: () => _removeChoice(index));
+      },
+    );
   }
 }
