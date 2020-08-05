118b4e82a28b02844c975748b58e5bdd84de6c6a
Alexander Zenner <alexander.zenner@student.hpi.de>
2020-08-05 16:43:53 +0200

    turn question card into generic study designer card

--- study_designer/lib/widgets/question_edit_widget.dart
+++ study_designer/lib/widgets/question_edit_widget.dart
@@ -4,10 +4,9 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:studyou_core/models/models.dart';
 
 class QuestionEditWidget extends StatefulWidget {
-  final Question item;
-  final void Function() remove;
+  final Question question;
 
-  const QuestionEditWidget({@required this.item, @required this.remove, Key key}) : super(key: key);
+  const QuestionEditWidget({@required this.question, Key key}) : super(key: key);
 
   @override
   _QuestionEditWidgetState createState() => _QuestionEditWidgetState();
@@ -18,48 +17,36 @@ class _QuestionEditWidgetState extends State<QuestionEditWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      ButtonBar(
-        children: <Widget>[
-          FlatButton(
-            onPressed: () {
-              widget.remove();
-            },
-            child: const Text('Delete'),
-          ),
-        ],
-      ),
-      FormBuilder(
-          key: _editFormKey,
-          autovalidate: true,
-          // readonly: true,
-          child: Column(children: <Widget>[
-            FormBuilderTextField(
-                validator: FormBuilderValidators.minLength(context, 3),
-                onChanged: (value) {
-                  saveFormChanges();
-                },
-                attribute: 'prompt',
-                decoration: InputDecoration(labelText: 'Prompt'),
-                initialValue: widget.item.prompt),
-            FormBuilderTextField(
-                validator: FormBuilderValidators.minLength(context, 3),
-                onChanged: (value) {
-                  saveFormChanges();
-                },
-                attribute: 'rationale',
-                decoration: InputDecoration(labelText: 'Rationale'),
-                initialValue: widget.item.rationale),
-          ])),
-    ]);
+    return FormBuilder(
+        key: _editFormKey,
+        autovalidate: true,
+        // readonly: true,
+        child: Column(children: <Widget>[
+          FormBuilderTextField(
+              validator: FormBuilderValidators.minLength(context, 3),
+              onChanged: (value) {
+                saveFormChanges();
+              },
+              attribute: 'prompt',
+              decoration: InputDecoration(labelText: 'Prompt'),
+              initialValue: widget.question.prompt),
+          FormBuilderTextField(
+              validator: FormBuilderValidators.minLength(context, 3),
+              onChanged: (value) {
+                saveFormChanges();
+              },
+              attribute: 'rationale',
+              decoration: InputDecoration(labelText: 'Rationale'),
+              initialValue: widget.question.rationale),
+        ]));
   }
 
   void saveFormChanges() {
     _editFormKey.currentState.save();
     if (_editFormKey.currentState.validate()) {
       setState(() {
-        widget.item.prompt = _editFormKey.currentState.value['prompt'];
-        widget.item.rationale = _editFormKey.currentState.value['rationale'];
+        widget.question.prompt = _editFormKey.currentState.value['prompt'];
+        widget.question.rationale = _editFormKey.currentState.value['rationale'];
       });
     }
   }
--- study_designer/lib/widgets/question_show_widget.dart
+++ study_designer/lib/widgets/question_show_widget.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:studyou_core/models/models.dart';
 
 class QuestionShowWidget extends StatelessWidget {
-  final Question item;
+  final Question question;
 
-  const QuestionShowWidget({@required this.item, Key key}) : super(key: key);
+  const QuestionShowWidget({@required this.question, Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(title: Text(item.prompt.isEmpty ? '*Click to edit*' : item.prompt));
+    return ListTile(title: Text(question.prompt.isEmpty ? '*Click to edit*' : question.prompt));
   }
 }
