796b5934da83aaaf70db2a68a559a68d86fb0093
Alexander Zenner <alexander.zenner@student.hpi.de>
2020-08-06 11:56:00 +0200

    remove click to edit from interventions

--- study_designer/lib/designer/interventions_designer/intervention_card.dart
+++ study_designer/lib/designer/interventions_designer/intervention_card.dart
@@ -1,33 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:provider/provider.dart';
 import 'package:study_designer/designer/interventions_designer/task_card.dart';
-import 'package:study_designer/models/designer_state.dart';
 import 'package:studyou_core/models/interventions/intervention.dart';
 import 'package:studyou_core/models/interventions/interventions.dart';
 import 'package:uuid/uuid.dart';
 
 class InterventionCard extends StatefulWidget {
-  final int interventionIndex;
-  final bool isEditing;
-  final void Function(int interventionIndex) remove;
-  final void Function(int interventionIndex) onTap;
+  final Intervention intervention;
+  final void Function() remove;
 
-  const InterventionCard(
-      {@required this.interventionIndex,
-      @required this.remove,
-      @required this.isEditing,
-      @required this.onTap,
-      Key key})
-      : super(key: key);
+  const InterventionCard({@required this.intervention, @required this.remove, Key key}) : super(key: key);
 
   @override
   _InterventionCardState createState() => _InterventionCardState();
 }
 
 class _InterventionCardState extends State<InterventionCard> {
-  Intervention intervention;
   int selectedTaskIndex;
 
   final GlobalKey<FormBuilderState> _editFormKey = GlobalKey<FormBuilderState>();
@@ -37,15 +26,14 @@ class _InterventionCardState extends State<InterventionCard> {
       final task = CheckmarkTask()
         ..id = Uuid().v4()
         ..title = '';
-      intervention.tasks.add(task);
-      selectedTaskIndex = intervention.tasks.length - 1;
+      widget.intervention.tasks.add(task);
     });
   }
 
   void _removeTask(taskIndex) {
     setState(() {
       selectedTaskIndex = null;
-      intervention.tasks.removeAt(taskIndex);
+      widget.intervention.tasks.removeAt(taskIndex);
     });
   }
 
@@ -53,36 +41,21 @@ class _InterventionCardState extends State<InterventionCard> {
     setState(() {
       selectedTaskIndex = index;
     });
-    widget.onTap(widget.interventionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    intervention =
-        context.watch<DesignerModel>().draftStudy.studyDetails.interventionSet.interventions[widget.interventionIndex];
-
     final cardContent = <Widget>[];
-    cardContent.add(Text('Intervention ${(widget.interventionIndex + 1).toString()}'));
-    if (widget.isEditing) {
-      cardContent.add(_buildDeleteButton());
-    }
-    if (widget.isEditing && selectedTaskIndex == null) {
-      cardContent.add(_buildEditMetaDataForm());
-    } else {
-      cardContent.add(_buildShowMetaData());
-    }
-    cardContent.addAll(_buildTaskCards());
-    if (widget.isEditing) {
-      cardContent.add(_buildCardFooter());
-    }
 
-    return GestureDetector(
-      onTap: () {
-        setState(() => selectedTaskIndex = null);
-        widget.onTap(widget.interventionIndex);
-      },
-      child: Card(margin: EdgeInsets.all(10.0), child: Column(children: cardContent)),
-    );
+    cardContent.add(_buildDeleteButton());
+
+    cardContent.add(_buildEditMetaDataForm());
+
+    cardContent.addAll(_buildTaskCards());
+
+    cardContent.add(_buildCardFooter());
+
+    return Card(margin: EdgeInsets.all(10.0), child: Column(children: cardContent));
   }
 
   Widget _buildDeleteButton() {
@@ -90,7 +63,7 @@ class _InterventionCardState extends State<InterventionCard> {
       children: <Widget>[
         FlatButton(
           onPressed: () {
-            widget.remove(widget.interventionIndex);
+            widget.remove();
           },
           child: const Text('Delete'),
         ),
@@ -99,15 +72,10 @@ class _InterventionCardState extends State<InterventionCard> {
   }
 
   List<Widget> _buildTaskCards() {
-    return intervention.tasks
+    return widget.intervention.tasks
         .asMap()
         .entries
-        .map((entry) => TaskCard(
-            interventionIndex: widget.interventionIndex,
-            taskIndex: entry.key,
-            remove: _removeTask,
-            isEditing: widget.isEditing && entry.key == selectedTaskIndex,
-            onTap: _selectTask))
+        .map((entry) => TaskCard(key: UniqueKey(), task: entry.value, remove: () => _removeTask(entry.key)))
         .toList();
   }
 
@@ -125,14 +93,14 @@ class _InterventionCardState extends State<InterventionCard> {
                 attribute: 'name',
                 maxLength: 40,
                 decoration: InputDecoration(labelText: 'Name'),
-                initialValue: intervention.name),
+                initialValue: widget.intervention.name),
             FormBuilderTextField(
                 onChanged: (value) {
                   saveFormChanges();
                 },
                 attribute: 'description',
                 decoration: InputDecoration(labelText: 'Description'),
-                initialValue: intervention.description),
+                initialValue: widget.intervention.description),
           ],
         ));
   }
@@ -141,17 +109,10 @@ class _InterventionCardState extends State<InterventionCard> {
     _editFormKey.currentState.save();
     if (_editFormKey.currentState.validate()) {
       setState(() {
-        intervention.name = _editFormKey.currentState.value['name'];
-        intervention.description = _editFormKey.currentState.value['description'];
+        widget.intervention.name = _editFormKey.currentState.value['name'];
+        widget.intervention.description = _editFormKey.currentState.value['description'];
       });
     }
-  }
-
-  Widget _buildShowMetaData() {
-    return ListTile(
-      title: Text(intervention.name.isEmpty ? 'Name' : intervention.name),
-      subtitle: Text(intervention.description.isEmpty ? 'Description' : intervention.description),
-    );
   }
 
   Widget _buildCardFooter() {
