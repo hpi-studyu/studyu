9cb544d0f945b2bf10702d128bb31832d58e24b3
Alexander Zenner <alexander.zenner@student.hpi.de>
2020-08-17 15:46:03 +0200

    add designer for eligibility criteria

--- study_designer/lib/designer/eligibility_designer.dart
+++ study_designer/lib/designer/eligibility_designer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:study_designer/widgets/eligibility/eligibility_criterion_editor.dart';
 import 'package:studyou_core/models/models.dart';
-import 'package:studyou_core/models/questionnaire/questionnaire.dart';
 import 'package:uuid/uuid.dart';
 
 import '../models/designer_state.dart';
@@ -13,25 +13,37 @@ class EligibilityDesigner extends StatefulWidget {
 }
 
 class _EligibilityDesignerState extends State<EligibilityDesigner> {
-  Questionnaire questionnaire;
+  List<EligibilityCriterion> _eligibility;
 
-  void _addQuestion() {
-    final question = BooleanQuestion()
-      ..id = Uuid().v4()
-      ..prompt = ''
-      ..rationale = '';
+  void _addCriterion() {
+    final criterion = EligibilityCriterion()..id = Uuid().v4();
     setState(() {
-      questionnaire.questions.add(question);
+      _eligibility.add(criterion);
+    });
+  }
+
+  void _removeEligibilityCriterion(index) {
+    setState(() {
+      _eligibility.removeAt(index);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    questionnaire = context.watch<DesignerModel>().draftStudy.studyDetails.questionnaire;
+    _eligibility = context.watch<DesignerState>().draftStudy.studyDetails.eligibility;
     return Stack(
       children: [
-        Center(child: Padding(padding: const EdgeInsets.all(16), child: Text('hi'))),
-        DesignerAddButton(label: Text('Add Criterion'), add: _addQuestion),
+        Center(
+            child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: SingleChildScrollView(
+                    child: Column(children: <Widget>[
+                  ..._eligibility.asMap().entries.map((entry) => EligibilityCriterionEditor(
+                      key: UniqueKey(),
+                      eligibilityCriterion: entry.value,
+                      remove: () => _removeEligibilityCriterion(entry.key)))
+                ])))),
+        DesignerAddButton(label: Text('Add Criterion'), add: _addCriterion),
       ],
     );
   }
