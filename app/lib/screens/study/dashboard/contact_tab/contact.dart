4cdaa957e30166ba10b28af9875cdf7e077ca950
Manisha Manaswini <manisha.manaswini@student.hpi.uni-potsdam.de>
2020-08-19 01:52:29 +0200

    changed colors

--- app/lib/screens/study/dashboard/contact_tab/contact.dart
+++ app/lib/screens/study/dashboard/contact_tab/contact.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../theme.dart';
 import '../../../../util/localization.dart';
 
 class Contact extends StatelessWidget {
@@ -14,17 +15,9 @@ class Contact extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RaisedButton(color: Colors.amber, onPressed: () {}, child: Text('FAQ')),
-            RaisedButton(
-              color: Colors.cyan,
-              onPressed: () {},
-              child: Text(Nof1Localizations.of(context).translate('contact')),
-            ),
-            RaisedButton(
-              color: Colors.amber,
-              onPressed: () {},
-              child: Text(Nof1Localizations.of(context).translate('about')),
-            ),
+            RaisedButton(color: theme.primaryColor, onPressed: () {}, child: Text('FAQ')),
+            RaisedButton(color: theme.primaryColor, onPressed: () {}, child: Text('Contact Support')),
+            RaisedButton(color: theme.primaryColor, onPressed: () {}, child: Text('Imprint/About')),
           ],
         ),
       ),
