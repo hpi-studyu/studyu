f221150079ce5853a867710d9fae52df87838dd1
Manisha Manaswini <manisha.manaswini@student.hpi.de>
2020-08-19 01:52:29 +0200

    changed colors

--- app/lib/screens/study/dashboard/contact_tab/contact.dart
+++ app/lib/screens/study/dashboard/contact_tab/contact.dart
@@ -1,8 +1,6 @@
-import 'package:StudYou/screens/app_onboarding/about.dart';
-import 'package:StudYou/screens/study/dashboard/contact_tab/contact_us.dart';
-import 'package:StudYou/screens/study/dashboard/contact_tab/faq.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../theme.dart';
 import '../../../../util/localization.dart';
 
 class Contact extends StatelessWidget {
@@ -10,58 +8,16 @@ class Contact extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(Nof1Localizations.of(context).translate('help')),
+        title: Text(Nof1Localizations.of(context).translate('contact')),
         centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const SizedBox(height: 30),
-            SizedBox(
-              width: 160,
-              height: 60,
-              child: RaisedButton(
-                  color: Colors.lightBlue[200],
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => FAQ()),
-                    );
-                  },
-                  child: Text('FAQ', style: TextStyle(fontSize: 18)),
-                  textColor: Colors.white),
-            ),
-            const SizedBox(height: 30),
-            SizedBox(
-              width: 160,
-              height: 60,
-              child: RaisedButton(
-                  color: Colors.blue[400],
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ContactUs()),
-                    );
-                  },
-                  child: Text(Nof1Localizations.of(context).translate('contact'), style: TextStyle(fontSize: 18)),
-                  textColor: Colors.white),
-            ),
-            const SizedBox(height: 30),
-            SizedBox(
-              width: 160,
-              height: 60,
-              child: RaisedButton(
-                  color: Colors.blue[600],
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => AboutScreen()),
-                    );
-                  },
-                  child: Text(Nof1Localizations.of(context).translate('about'), style: TextStyle(fontSize: 18)),
-                  textColor: Colors.white),
-            )
+            RaisedButton(color: theme.primaryColor, onPressed: () {}, child: Text('FAQ')),
+            RaisedButton(color: theme.primaryColor, onPressed: () {}, child: Text('Contact Support')),
+            RaisedButton(color: theme.primaryColor, onPressed: () {}, child: Text('Imprint/About')),
           ],
         ),
       ),
