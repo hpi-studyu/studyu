b0ca5658e1d2f76cbb2c169a3fe20110c90a7673
Nils Strelow <nils.strelow@student.hpi.de>
2020-08-22 21:21:25 +0200

    Extract StudyTile

--- app/lib/screens/study/onboarding/study_selection.dart
+++ app/lib/screens/study/onboarding/study_selection.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:provider/provider.dart';
 import 'package:studyou_core/models/models.dart';
 import 'package:studyou_core/util/parse_future_builder.dart';
 
-import '../../../models/app_state.dart';
-import '../../../routes.dart';
 import '../../../util/localization.dart';
 import '../../../widgets/bottom_onboarding_navigation.dart';
+import '../../../widgets/study_tile.dart';
 
 class StudySelectionScreen extends StatefulWidget {
   @override
@@ -22,11 +19,6 @@ class _StudySelectionScreenState extends State<StudySelectionScreen> {
   void initState() {
     super.initState();
     _studiesFuture = ParseStudy().getAll();
-  }
-
-  Future<void> navigateToStudyOverview(BuildContext context, ParseStudy selectedStudy) async {
-    context.read<AppModel>().selectedStudy = selectedStudy;
-    Navigator.pushNamed(context, Routes.studyOverview);
   }
 
   @override
@@ -82,18 +74,7 @@ class _StudySelectionScreenState extends State<StudySelectionScreen> {
                         physics: NeverScrollableScrollPhysics(),
                         itemCount: studies.length,
                         itemBuilder: (context, index) {
-                          final currentStudy = studies[index];
-                          return ListTile(
-                              contentPadding: EdgeInsets.all(16),
-                              onTap: () {
-                                navigateToStudyOverview(context, currentStudy);
-                              },
-                              title: Center(
-                                  child: Text(currentStudy.title,
-                                      style: theme.textTheme.headline6.copyWith(color: theme.primaryColor))),
-                              subtitle: Center(child: Text(currentStudy.description)),
-                              leading: Icon(MdiIcons.fromString(currentStudy.iconName ?? 'accountHeart'),
-                                  color: theme.primaryColor));
+                          return StudyTile(study: studies[index]);
                         });
                   },
                 ),
---
+++ app/lib/widgets/study_tile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:studyou_core/models/models.dart';
+
+import '../models/app_state.dart';
+import '../routes.dart';
+
+class StudyTile extends StatelessWidget {
+  final StudyBase study;
+
+  const StudyTile({@required this.study, Key key}) : super(key: key);
+
+  Future<void> navigateToStudyOverview(BuildContext context, StudyBase selectedStudy) async {
+    context.read<AppModel>().selectedStudy = selectedStudy;
+    Navigator.pushNamed(context, Routes.studyOverview);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return ListTile(
+        contentPadding: EdgeInsets.all(16),
+        onTap: () {
+          navigateToStudyOverview(context, study);
+        },
+        title: Center(child: Text(study.title, style: theme.textTheme.headline6.copyWith(color: theme.primaryColor))),
+        subtitle: Center(child: Text(study.description)),
+        leading: Icon(MdiIcons.fromString(study.iconName ?? 'accountHeart'), color: theme.primaryColor));
+  }
+}
