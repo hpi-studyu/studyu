1f9ebe477d344b82c49b9026b54b73463c45ff51
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-08-23 17:07:08 +0200

    Integrate data reference methods

--- core/lib/models/observations/tasks/questionnaire_task.dart
+++ core/lib/models/observations/tasks/questionnaire_task.dart
@@ -1,6 +1,9 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import '../../questionnaire/question.dart';
 import '../../questionnaire/questionnaire.dart';
+import '../../questionnaire/questionnaire_state.dart';
+import '../../results/result.dart';
 import '../../tasks/schedule.dart';
 import '../observation.dart';
 
@@ -18,4 +21,13 @@ class QuestionnaireTask extends Observation {
 
   @override
   Map<String, dynamic> toJson() => _$QuestionnaireTaskToJson(this);
+
+  @override
+  Map<DateTime, T> extractPropertyResults<T>(String property, List<Result> sourceResults) {
+    Question targetQuestion = questions.questions.firstWhere((q) => q.id == property, orElse: null);
+    if (targetQuestion == null)
+      throw new ArgumentError('Questionnaire \'${this.id}\' does not have a question with \'$property\'.');
+    var typedResults = sourceResults as List<Result<QuestionnaireState>>;
+    return Map.fromEntries(typedResults.map((r) => new MapEntry(r.timeStamp, r.result.getAnswer<T>(property))));
+  }
 }
