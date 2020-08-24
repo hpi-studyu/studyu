c4ed8e13f2a41d11a01440aeeadbdcd2b3107853
Alexander Zenner <alexander.zenner@student.hpi.de>
2020-08-24 14:19:58 +0200

    add is multiple editor

--- study_designer/lib/widgets/question/choice_question_editor_section.dart
+++ study_designer/lib/widgets/question/choice_question_editor_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:studyou_core/models/models.dart';
 import 'package:uuid/uuid.dart';
 
@@ -15,6 +16,8 @@ class ChoiceQuestionEditorSection extends StatefulWidget {
 }
 
 class _ChoiceQuestionEditorSectionState extends State<ChoiceQuestionEditorSection> {
+  final GlobalKey<FormBuilderState> _editFormKey = GlobalKey<FormBuilderState>();
+
   void _addChoice() {
     setState(() {
       final choice = Choice()
@@ -33,6 +36,18 @@ class _ChoiceQuestionEditorSectionState extends State<ChoiceQuestionEditorSectio
   @override
   Widget build(BuildContext context) {
     return Column(children: [
+      FormBuilder(
+          key: _editFormKey,
+          autovalidate: true,
+          // readonly: true,
+          child: Column(children: <Widget>[
+            FormBuilderSwitch(
+              onChanged: _saveFormChanges,
+              title: Text('Multiple'),
+              name: 'multiple',
+              initialValue: widget.question.multiple,
+            ),
+          ])),
       ListView.builder(
         shrinkWrap: true,
         itemCount: widget.question.choices.length,
@@ -47,5 +62,14 @@ class _ChoiceQuestionEditorSectionState extends State<ChoiceQuestionEditorSectio
         Spacer()
       ])
     ]);
+  }
+
+  void _saveFormChanges(value) {
+    _editFormKey.currentState.save();
+    if (_editFormKey.currentState.validate()) {
+      setState(() {
+        widget.question.multiple = _editFormKey.currentState.value['multiple'];
+      });
+    }
   }
 }
