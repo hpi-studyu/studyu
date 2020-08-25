58cb39d41bde0ce30e2f2603ed85f72e61cd1595
Nils Strelow <nils.strelow@student.hpi.de>
2020-08-26 01:29:50 +0200

    Make journey screen nice

--- app/lib/screens/study/onboarding/journey_overview.dart
+++ app/lib/screens/study/onboarding/journey_overview.dart
@@ -1,9 +1,10 @@
-import 'package:StudYou/widgets/study_tile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:studyou_core/models/models.dart';
+import 'package:timeline_tile/timeline_tile.dart';
 
 import '../../../models/app_state.dart';
 import '../../../routes.dart';
@@ -37,48 +38,140 @@ class _JourneyOverviewScreen extends State<JourneyOverviewScreen> {
     study = context.read<AppState>().activeStudy;
   }
 
-  List<Widget> buildJourney() {
-    return study.interventionOrder
-        .map((id) => Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(children: [
-              Text(study.interventionSet.interventions
-                      .firstWhere((intervention) => intervention.id == id, orElse: () => null)
-                      ?.name ??
-                  '')
-            ])))
-        .toList();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Nof1Localizations.of(context).translate('your_journey')),
+        leading: Icon(MdiIcons.mapMarkerPath),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                //StudyTile.fromUserStudy(study: study),
+                Timeline(study: study),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomOnboardingNavigation(
+        onNext: () => getConsentAndNavigateToDashboard(context),
+        progress: OnboardingProgress(stage: 2, progress: 0.5),
+      ),
+    );
   }
+}
+
+class Timeline extends StatelessWidget {
+  final UserStudyBase study;
+
+  const Timeline({@required this.study, Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final interventionsInOrder = study.getInterventionsInOrder();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ...interventionsInOrder.asMap().entries.map((entry) {
+          final index = entry.key;
+          final intervention = entry.value;
+          return InterventionTile(
+            title: intervention.name,
+            iconName: intervention.icon,
+            date: study.startDate.add(Duration(days: index * study.schedule.phaseDuration)),
+            isFirst: index == 0,
+          );
+        }).toList(),
+        InterventionTile(
+            title: Nof1Localizations.of(context).translate('journey_results_available'),
+            iconName: 'flagCheckered',
+            color: Colors.green,
+            isLast: true,
+            date: study.startDate.add(Duration(days: interventionsInOrder.length * study.schedule.phaseDuration)))
+      ],
+    );
+  }
+}
+
+class InterventionTile extends StatelessWidget {
+  final String title;
+  final String iconName;
+  final DateTime date;
+  final Color color;
+  final bool isFirst;
+  final bool isLast;
+
+  const InterventionTile(
+      {@required this.title,
+      @required this.iconName,
+      @required this.date,
+      this.color,
+      this.isFirst = false,
+      this.isLast = false,
+      Key key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(study.title),
-        leading: Icon(MdiIcons.fromString(study.iconName)),
+    return TimelineTile(
+      alignment: TimelineAlign.manual,
+      lineX: 0.4,
+      isFirst: isFirst,
+      isLast: isLast,
+      indicatorStyle: IndicatorStyle(
+        width: 40,
+        height: 40,
+        padding: const EdgeInsets.all(8),
+        indicator: IconIndicator(iconName: iconName, color: color),
       ),
-      body: Builder(builder: (_context) {
-        return Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  StudyTile.fromUserStudy(study: study),
-                  SizedBox(height: 40),
-                  ...buildJourney(),
-                ],
-              ),
-            ),
-          ),
-        );
-      }),
-      bottomNavigationBar: BottomOnboardingNavigation(
-        onNext: () => getConsentAndNavigateToDashboard(context),
-        progress: OnboardingProgress(stage: 2, progress: 0.5),
+      topLineStyle: LineStyle(color: theme.primaryColor),
+      bottomLineStyle: LineStyle(color: theme.primaryColor),
+      rightChild: TimelineChild(
+        child: Text(title, style: theme.textTheme.headline6.copyWith(color: theme.primaryColor)),
+      ),
+      leftChild: TimelineChild(
+        child: Text(DateFormat('dd-MM-yyyy').format(date), style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
+    );
+  }
+}
+
+class IconIndicator extends StatelessWidget {
+  final String iconName;
+  final Color color;
+
+  const IconIndicator({@required this.iconName, this.color, Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(shape: BoxShape.circle, color: color ?? Theme.of(context).accentColor),
+      child: Center(
+        child: Icon(MdiIcons.fromString(iconName), color: Colors.white),
+      ),
+    );
+  }
+}
+
+class TimelineChild extends StatelessWidget {
+  final Widget child;
+
+  const TimelineChild({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      constraints: const BoxConstraints(minHeight: 100),
+      child: Center(
+        child: child,
       ),
     );
   }
