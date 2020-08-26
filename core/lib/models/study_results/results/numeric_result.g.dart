b25582e0b02dce46141813ab59cb4fb66f87a05c
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-08-26 09:26:05 +0200

    Implement numeric data export

---
+++ core/lib/models/study_results/results/numeric_result.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'numeric_result.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+NumericResult _$NumericResultFromJson(Map<String, dynamic> json) {
+  return NumericResult()
+    ..type = json['type'] as String
+    ..id = json['id'] as String
+    ..filename = json['filename'] as String
+    ..resultProperty =
+        DataReference.fromJson(json['resultProperty'] as Map<String, dynamic>);
+}
+
+Map<String, dynamic> _$NumericResultToJson(NumericResult instance) =>
+    <String, dynamic>{
+      'type': instance.type,
+      'id': instance.id,
+      'filename': instance.filename,
+      'resultProperty': instance.resultProperty.toJson(),
+    };
