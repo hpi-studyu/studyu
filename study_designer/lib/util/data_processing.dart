7e43c2365e21da6733ade1948ecf8d6a09fa1ddd
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-08-28 19:14:10 +0200

    Add data processing utilities

---
+++ study_designer/lib/util/data_processing.dart
@@ -0,0 +1,36 @@
+typedef FoldAggregator<V, R> = R Function(Iterable<V>);
+typedef KeyAccessor<K, V> = K Function(V);
+
+class GroupedIterable<K, V> extends Iterable<MapEntry<K, Iterable<V>>> {
+  Map<K, Iterable<V>> data;
+
+  GroupedIterable() : data = {};
+  GroupedIterable.from(this.data);
+
+  @override
+  Iterator<MapEntry<K, Iterable<V>>> get iterator => data.entries.iterator;
+
+  Iterable<MapEntry<K, R>> aggregateFold<R>(R Function(Iterable<V>) aggregator) =>
+      map((entry) => MapEntry(entry.key, aggregator(entry.value)));
+}
+
+class FoldAggregators {
+  static FoldAggregator<V, V> min<V extends Comparable>() =>
+      (values) => values.reduce((a, b) => a.compareTo(b) < 0 ? a : b);
+  static FoldAggregator<V, V> median<V extends Comparable>() => (values) {
+        final list = values.toList(growable: false)..sort();
+        return list[list.length ~/ 2];
+      };
+  static FoldAggregator<V, V> max<V extends Comparable>() =>
+      (values) => values.reduce((a, b) => a.compareTo(b) > 0 ? a : b);
+  static FoldAggregator<num, num> sum() => (values) => values.reduce((value, element) => value + element);
+  static FoldAggregator<num, num> mean() => (values) => sum()(values) / values.length;
+}
+
+extension GroupByIterable<V> on Iterable<V> {
+  GroupedIterable<K, V> groupBy<K>(KeyAccessor<K, V> key) {
+    final result = <K, List<V>>{};
+    forEach((element) => result.putIfAbsent(key(element), () => []).add(element));
+    return GroupedIterable.from(result);
+  }
+}
