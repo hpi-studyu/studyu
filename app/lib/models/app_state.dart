b6330f9cecf84214cfcc0e175b8764d7ea69a337
Nils Strelow <strelow.nils@gmail.com>
2020-10-22 00:08:11 +0200

    fix: Fix contact not displaying correctly
    also refactoring

--- app/lib/models/app_state.dart
+++ app/lib/models/app_state.dart
@@ -1,13 +1,11 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:studyou_core/models/models.dart';
-import 'package:studyou_core/models/study/contact.dart';
 
 class AppState {
   ParseStudy selectedStudy;
   List<Intervention> selectedInterventions;
   ParseUserStudy activeStudy;
   FlutterLocalNotificationsPlugin notificationsPlugin;
-  Contact appSupportContact;
 
   AppState(this.activeStudy);
 }
--- app/lib/screens/study/dashboard/contact_tab/contact_screen.dart
+++ app/lib/screens/study/dashboard/contact_tab/contact_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:provider/provider.dart';
 import 'package:studyou_core/models/study/contact.dart';
 import 'package:studyou_core/util/localization.dart';
+import 'package:studyou_core/util/retry_future_builder.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../models/app_state.dart';
@@ -13,22 +15,28 @@ class ContactScreen extends StatefulWidget {
 }
 
 class _ContactScreenState extends State<ContactScreen> {
-  Contact appSupportContact;
+  Future<Contact> appSupportContactFuture;
   Contact studyContact;
 
   @override
   void initState() {
     super.initState();
-    appSupportContact = context.read<AppState>().appSupportContact;
-    print(appSupportContact);
-    studyContact = context.read<AppState>().activeStudy.contact;
-    print(studyContact);
+    appSupportContactFuture = getParseConfigContact();
+    studyContact = context.read<AppState>().activeStudy?.contact;
+  }
+
+  Future<Contact> getParseConfigContact() async {
+    final configs = await ParseConfig().getConfigs();
+    return Contact.fromJson(configs.result['contact']);
   }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
+      appBar: AppBar(
+        title: Text(Nof1Localizations.of(context).translate('contact')),
+      ),
       body: ListView(
         children: <Widget>[
           Container(
@@ -38,91 +46,88 @@ class _ContactScreenState extends State<ContactScreen> {
               height: 80,
             ),
           ),
-          Visibility(
-              visible: appSupportContact != null,
-              child: Column(
-                children: [
-                  Text('App support', style: theme.textTheme.headline6.copyWith(color: theme.primaryColor)),
-                  Text('Contact for problems or questions with the app',
-                      style: theme.textTheme.subtitle1.copyWith(fontSize: 14)),
-                ],
-              )),
-          ContactItem(
-            itemName: 'Organization',
-            itemValue: appSupportContact?.organization,
-            iconData: MdiIcons.hospitalBuilding,
-          ),
-          ContactItem(
-            itemName: 'Researchers',
-            itemValue: appSupportContact?.researchers,
-            iconData: MdiIcons.doctor,
-          ),
-          ContactItem(
-            itemName: 'Website',
-            itemValue: appSupportContact?.website,
-            iconData: MdiIcons.web,
-            type: ContactItemType.website,
-          ),
-          ContactItem(
-            itemName: 'Email',
-            itemValue: appSupportContact?.email,
-            iconData: MdiIcons.email,
-            type: ContactItemType.email,
-          ),
-          ContactItem(
-            itemName: 'Phone',
-            itemValue: appSupportContact?.phone,
-            iconData: MdiIcons.phone,
-            type: ContactItemType.phone,
-          ),
+          RetryFutureBuilder(
+              tryFunction: getParseConfigContact,
+              successBuilder: (context, appSupportContact) => ContactWidget(
+                    contact: appSupportContact,
+                    title: 'App support',
+                    subtitle: 'Contact for problems or questions with the app',
+                    color: theme.primaryColor,
+                  )),
           SizedBox(height: 20),
-          Visibility(
-              visible: appSupportContact != null,
-              child: Column(
-                children: [
-                  Text('Study support', style: theme.textTheme.headline6.copyWith(color: theme.accentColor)),
-                  Text('Contact for problems or questions with the study',
-                      style: theme.textTheme.subtitle1.copyWith(fontSize: 14)),
-                ],
-              )),
-          ContactItem(
-            itemName: 'Organization',
-            itemValue: studyContact?.organization,
-            iconData: MdiIcons.hospitalBuilding,
-            iconColor: theme.accentColor,
-          ),
-          ContactItem(
-            itemName: 'Researchers',
-            itemValue: studyContact?.researchers,
-            iconData: MdiIcons.doctor,
-            iconColor: theme.accentColor,
-          ),
-          ContactItem(
-            itemName: 'Website',
-            itemValue: studyContact?.website,
-            iconData: MdiIcons.web,
-            type: ContactItemType.website,
-            iconColor: theme.accentColor,
-          ),
-          ContactItem(
-            itemName: 'Email',
-            itemValue: studyContact?.email,
-            iconData: MdiIcons.email,
-            type: ContactItemType.email,
-            iconColor: theme.accentColor,
-          ),
-          ContactItem(
-            itemName: 'Phone',
-            itemValue: studyContact?.phone,
-            iconData: MdiIcons.phone,
-            type: ContactItemType.phone,
-            iconColor: theme.accentColor,
+          ContactWidget(
+            contact: studyContact,
+            title: 'Study support',
+            subtitle: 'Contact for problems or questions with the study',
+            color: theme.accentColor,
           ),
         ],
       ),
-      appBar: AppBar(
-        title: Text(Nof1Localizations.of(context).translate('Contact Us')),
-      ),
+    );
+  }
+}
+
+class ContactWidget extends StatelessWidget {
+  final Contact contact;
+  final String title;
+  final String subtitle;
+  final Color color;
+
+  const ContactWidget({@required this.contact, @required this.title, @required this.color, this.subtitle, Key key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    if (contact == null) {
+      return Container();
+    }
+
+    final titles = [Text(title, style: theme.textTheme.headline6.copyWith(color: color))];
+    if (subtitle != null && subtitle.isNotEmpty) {
+      titles.add(Text(subtitle, style: theme.textTheme.subtitle1.copyWith(fontSize: 14)));
+    }
+
+    return Column(
+      children: [
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: titles,
+        ),
+        ContactItem(
+          itemName: 'Organization',
+          itemValue: contact?.organization,
+          iconData: MdiIcons.hospitalBuilding,
+          iconColor: color,
+        ),
+        ContactItem(
+          itemName: 'Researchers',
+          itemValue: contact?.researchers,
+          iconData: MdiIcons.doctor,
+          iconColor: color,
+        ),
+        ContactItem(
+          itemName: 'Website',
+          itemValue: contact?.website,
+          iconData: MdiIcons.web,
+          type: ContactItemType.website,
+          iconColor: color,
+        ),
+        ContactItem(
+          itemName: 'Email',
+          itemValue: contact?.email,
+          iconData: MdiIcons.email,
+          type: ContactItemType.email,
+          iconColor: color,
+        ),
+        ContactItem(
+          itemName: 'Phone',
+          itemValue: contact?.phone,
+          iconData: MdiIcons.phone,
+          type: ContactItemType.phone,
+          iconColor: color,
+        ),
+      ],
     );
   }
 }
@@ -140,36 +145,37 @@ class ContactItem extends StatelessWidget {
       {@required this.itemName, @required this.itemValue, @required this.iconData, this.type, this.iconColor, Key key})
       : super(key: key);
 
+  void launchContact() {
+    {
+      switch (type) {
+        case ContactItemType.website:
+          if (!itemValue.startsWith('http://') && !itemValue.startsWith('https://')) {
+            launch('http://$itemValue');
+          } else {
+            launch(itemValue);
+          }
+          break;
+        case ContactItemType.email:
+          launch('mailto:$itemValue');
+          break;
+        case ContactItemType.phone:
+          launch('tel:$itemValue');
+          break;
+      }
+      launch(itemValue);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (itemValue == null || itemValue.isEmpty) return Container();
+
     const iconSize = 40.0;
-    return Visibility(
-      visible: itemValue != null && itemValue.isNotEmpty,
-      child: ListTile(
-        title: Text(itemName),
-        subtitle: SelectableText(itemValue ?? ''),
-        leading: Icon(iconData, color: iconColor ?? Theme.of(context).primaryColor, size: iconSize),
-        onTap: type != null
-            ? () {
-                switch (type) {
-                  case ContactItemType.website:
-                    if (!itemValue.startsWith('http://') && !itemValue.startsWith('https://')) {
-                      launch('http://$itemValue');
-                    } else {
-                      launch(itemValue);
-                    }
-                    break;
-                  case ContactItemType.email:
-                    launch('mailto:$itemValue');
-                    break;
-                  case ContactItemType.phone:
-                    launch('tel:$itemValue');
-                    break;
-                }
-                launch(itemValue);
-              }
-            : null,
-      ),
+    return ListTile(
+      title: Text(itemName),
+      subtitle: SelectableText(itemValue),
+      leading: Icon(iconData, color: iconColor ?? Theme.of(context).primaryColor, size: iconSize),
+      onTap: type != null ? launchContact : null,
     );
   }
 }
