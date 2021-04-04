3914c9d43d6b4ad89385cdbdec5b5f419ca9f29c
Nils Strelow <strelow.nils@gmail.com>
2021-04-04 13:36:11 +0200

    feat: Implement conditional FHIR questions

--- app/lib/widgets/fhir_questionnaire/questionnaire_widget.dart
+++ app/lib/widgets/fhir_questionnaire/questionnaire_widget.dart
@@ -2,12 +2,11 @@ import 'dart:async';
 
 import 'package:fhir/r4.dart' as fhir;
 import 'package:flutter/material.dart';
-import 'package:studyou_core/models/models.dart';
 
 import 'question_container.dart';
 
-typedef StateHandler = void Function(QuestionnaireState);
-typedef ContinuationPredicate = bool Function(QuestionnaireState);
+typedef StateHandler = void Function(fhir.QuestionnaireResponse);
+typedef ContinuationPredicate = bool Function(fhir.QuestionnaireResponse);
 
 class FhirQuestionnaireWidget extends StatefulWidget {
   final String title;
@@ -32,10 +31,9 @@ class _FhirQuestionnaireWidgetState extends State<FhirQuestionnaireWidget> {
   // Map of question -> Answer
   // Answer can have type bool, num(int,double), String, List<String>
   // Only bool and num are currently used and List<String> (choice, only used for eligibility)
-  final QuestionnaireState qs = QuestionnaireState(); // equivalent to QuestionnaireResponse
   fhir.QuestionnaireResponse questionnaireResponse;
 
-  void _finishQuestionnaire() => widget.onComplete?.call(qs);
+  void _finishQuestionnaire() => widget.onComplete?.call(questionnaireResponse);
 
   /// Removes responses of clicked question and all following questions
   void _invalidateDownstreamAnswers(int index) {
@@ -73,15 +71,14 @@ class _FhirQuestionnaireWidgetState extends State<FhirQuestionnaireWidget> {
       questionnaireResponse.item.add(newResponseItem);
     }
 
-    widget.onChange?.call(qs);
-    if (widget.questionnaire.item.length > shownQuestions.length) {
-      if (!(widget.shouldContinue?.call(qs) ?? true)) return;
-      // TODO: Check if next question should be show based on answer of previous question (FHIR Questionnaire Pane)
-      // if (!widget.questionnaire.item[_nextQuestionIndex].shouldBeShown(qs)) {
-      //   _onQuestionDone(widget.questionnaire.item[_nextQuestionIndex].getDefaultAnswer(), shownQuestions.length);
-      //   return;
-      // }
-      _insertQuestion(widget.questionnaire.item[shownQuestions.length]);
+    widget.onChange?.call(questionnaireResponse);
+    final nextQuestion = _nextQuestion();
+    print(nextQuestion);
+    if (nextQuestion != null) {
+      if (!(widget.shouldContinue?.call(questionnaireResponse) ?? true)) {
+        return; // Checks if answer is wrong => not eligible ==> VALIDATION OF ANSWERS GIVEN!
+      }
+      _insertQuestion(nextQuestion);
       _listKey.currentState.insertItem(shownQuestions.length - 1, duration: Duration(milliseconds: 300));
 
       // Scroll to bottom
@@ -98,6 +95,50 @@ class _FhirQuestionnaireWidgetState extends State<FhirQuestionnaireWidget> {
     } else {
       _finishQuestionnaire();
     }
+  }
+
+  fhir.QuestionnaireItem _nextQuestion() {
+    // TODO: Use Map<linkId, QuestionnaireItem> to not iterate over all questions
+    return widget.questionnaire.item
+        .firstWhere((item) => isQuestionNotDisplayedYet(item) && isQuestionEnabled(item), orElse: () => null);
+  }
+
+  bool isQuestionNotDisplayedYet(fhir.QuestionnaireItem item) {
+    return shownQuestions.every((question) => question.question.linkId != item.linkId);
+  }
+
+  bool isQuestionEnabled(fhir.QuestionnaireItem item) {
+    if (item.enableWhen == null || item.enableWhen.isEmpty) return true;
+
+    switch (item.enableBehavior) {
+      case fhir.QuestionnaireItemEnableBehavior.all:
+        return item.enableWhen.every(_satisfies);
+      case fhir.QuestionnaireItemEnableBehavior.any:
+        return item.enableWhen.any(_satisfies);
+      default:
+        return true;
+    }
+  }
+
+  bool _satisfies(fhir.QuestionnaireEnableWhen condition) {
+    final conditionalItem =
+        questionnaireResponse.item?.firstWhere((item) => item.linkId == condition.question, orElse: () => null);
+    switch (condition.operator_) {
+      case fhir.QuestionnaireEnableWhenOperator.exists:
+        if (conditionalItem == null) {
+          return false;
+        }
+        break;
+      case fhir.QuestionnaireEnableWhenOperator.eq:
+        final responseCoding = conditionalItem?.answer?.first?.valueCoding;
+        if (responseCoding?.code != condition.answerCoding?.code) {
+          return false;
+        }
+        break;
+      default:
+        print('Unsupported operator: ${condition.operator_}.');
+    }
+    return true;
   }
 
   @override
