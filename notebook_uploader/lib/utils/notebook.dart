cbd5d6f8824e041dcfee15371c1089297b89a1ab
Nils Strelow <strelow.nils@gmail.com>
2021-05-09 14:29:53 +0200

    fix(uploader): Check if file already present

--- notebook_uploader/lib/utils/notebook.dart
+++ notebook_uploader/lib/utils/notebook.dart
@@ -7,25 +7,25 @@ Future<void> uploadNotebookToSupabase(String filePath, String studyId) async {
   final file = File(filePath);
   final fileName = p.basename(file.path);
 
-  await _uploadToSupabaseStorage('$studyId/$fileName', file, 'notebook-widgets');
+  await _uploadToSupabaseStorage(studyId, fileName, file, 'notebook-widgets');
 }
 
-Future<void> _uploadToSupabaseStorage(String filePath, File file, String bucketId) async {
-  final res = await env.client.storage.from(bucketId).update(filePath, file);
-  print(res.data);
+Future<void> _uploadToSupabaseStorage(String studyId, String fileName, File file, String bucketId) async {
+  final listFilesResponse = await env.client.storage.from(bucketId).list(path: studyId);
+  print(listFilesResponse.data);
+  if (listFilesResponse.hasError) {
+    print(listFilesResponse.error!.message);
+    exit(1);
+  }
 
-  if (res.hasError) {
-    if (res.error!.statusCode == '404') {
-      print('Notebook does not exist. Use upload instead of update');
-      final res = await env.client.storage.from(bucketId).upload(filePath, file);
-      print(res.data);
-      if (res.hasError) {
-        print(res.error!.message);
-        exit(1);
-      }
-    } else {
-      print(res.error!.message);
-      exit(1);
-    }
+  final filePresent = listFilesResponse.data!.any((file) => file.name == fileName);
+  final updateResponse = filePresent
+      ? await env.client.storage.from(bucketId).update('$studyId/$fileName', file)
+      : await env.client.storage.from(bucketId).upload('$studyId/$fileName', file);
+  if (updateResponse.hasError) {
+    print(updateResponse.error!.message);
+    exit(1);
+  } else {
+    print(updateResponse.data);
   }
 }
