7e738c86c0a48b3c1bf24866744775e02371f738
Nils Strelow <strelow.nils@gmail.com>
2021-05-14 01:01:31 +0200

    chore: Upgrade from_builder, keeping ColorPicker

--- study_designer/lib/widgets/question/visual_analogue_question_editor_section.dart
+++ study_designer/lib/widgets/question/visual_analogue_question_editor_section.dart
@@ -3,6 +3,8 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:studyou_core/core.dart';
 
+import '../../util/form_builder_color_picker.dart';
+
 class VisualAnalogueQuestionEditorSection extends StatefulWidget {
   final VisualAnalogueQuestion question;
 
