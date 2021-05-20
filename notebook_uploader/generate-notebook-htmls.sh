1da42b3d5f4c41b75098ef14a5342a7b291509ac
Nils Strelow <strelow.nils@gmail.com>
2021-05-20 14:17:41 +0200

    fix: Add allow-errors to notebook html generation

--- notebook_uploader/generate-notebook-htmls.sh
+++ notebook_uploader/generate-notebook-htmls.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f -name \*.ipynb -exec bash -c 'FN="{}"; jupyter nbconvert --execute --to html "{}" --no-prompt --template /nbconvert-template; uploader -t "$session" -s $study_id "${FN%.ipynb}.html"' \;
+find . -type f -name \*.ipynb -exec bash -c 'FN="{}"; jupyter nbconvert --execute --to html "{}" --allow-errors --no-prompt --template /nbconvert-template; uploader -t "$session" -s $study_id "${FN%.ipynb}.html"' \;
