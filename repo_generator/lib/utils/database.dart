7ef51583a313645f84156050c24c4979a6ee75cf
Nils Strelow <strelow.nils@gmail.com>
2021-07-18 18:20:50 +0200

    feat: Export participant results as flattened CSV

--- repo_generator/lib/utils/database.dart
+++ repo_generator/lib/utils/database.dart
@@ -1,11 +1,5 @@
 import 'package:studyu_core/core.dart';
-import 'package:studyu_core/env.dart' as env;
 
 Future<Study> fetchStudySchema(String studyId) async => SupabaseQuery.getById<Study>(studyId);
 
-Future<List<dynamic>> fetchSubjects(String studyId) async {
-  final res =
-      await env.client.from(StudySubject.tableName).select('*,subject_progress(*)').eq('study_id', studyId).execute();
-  SupabaseQuery.catchPostgrestError(res);
-  return res.data;
-}
+Future<String> fetchSubjects(String studyId) async => Study.fetchResultsCSVTable(studyId);
