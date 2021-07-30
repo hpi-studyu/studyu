c6aba8243a3cd1eb3f81490ce18245fc5ccc2451
Nils Strelow <strelow.nils@gmail.com>
2021-07-30 14:06:49 +0200

    feat: Add ability to initialize with existing

--- core/lib/src/env/env.dart
+++ core/lib/src/env/env.dart
@@ -2,17 +2,17 @@ import 'package:supabase/supabase.dart';
 
 final dotEnv = <String, String>{};
 
-void loadEnv(Map<String, String> env) {
+late SupabaseClient client;
+late String supabaseUrl;
+late String supabaseAnonKey;
+late String appUrl;
+late String projectGeneratorUrl;
+
+void loadEnv(Map<String, String> env, {SupabaseClient? supabaseClient}) {
   dotEnv.addAll(env);
+  projectGeneratorUrl = dotEnv['STUDYU_PROJECT_GENERATOR_URL']!;
+  appUrl = dotEnv['STUDYU_APP_URL']!;
+  supabaseUrl = dotEnv['STUDYU_SUPABASE_URL']!;
+  supabaseAnonKey = dotEnv['STUDYU_SUPABASE_PUBLIC_ANON_KEY']!;
+  client = supabaseClient ?? SupabaseClient(supabaseUrl, supabaseAnonKey);
 }
-
-const designerDeepLink = 'https://designer.studyu.health';
-
-// GoTrue redirectTo strips /#/ from URL, therefore we need to use the default set in supabase
-String? authRedirectToUrl({bool isWeb = false}) => isWeb ? Uri.base.toString() : designerDeepLink;
-
-final projectGeneratorUrl = dotEnv['STUDYU_PROJECT_GENERATOR_URL']!;
-final appUrl = dotEnv['STUDYU_APP_URL']!;
-final supabaseUrl = dotEnv['STUDYU_SUPABASE_URL']!;
-final supabaseAnonKey = dotEnv['STUDYU_SUPABASE_PUBLIC_ANON_KEY']!;
-final client = SupabaseClient(supabaseUrl, supabaseAnonKey);
