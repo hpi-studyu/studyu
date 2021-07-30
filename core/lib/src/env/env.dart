b2187717d2d4fb71f77ccc79d7d8ac6e124c8911
Nils Strelow <strelow.nils@gmail.com>
2021-07-30 16:24:21 +0200

    fix: Make appUrl and projectGeneratorUrl nullable

--- core/lib/src/env/env.dart
+++ core/lib/src/env/env.dart
@@ -5,13 +5,13 @@ final dotEnv = <String, String>{};
 late SupabaseClient client;
 late String supabaseUrl;
 late String supabaseAnonKey;
-late String appUrl;
-late String projectGeneratorUrl;
+late String? appUrl;
+late String? projectGeneratorUrl;
 
 void loadEnv(Map<String, String> env, {SupabaseClient? supabaseClient}) {
   dotEnv.addAll(env);
-  projectGeneratorUrl = dotEnv['STUDYU_PROJECT_GENERATOR_URL']!;
-  appUrl = dotEnv['STUDYU_APP_URL']!;
+  projectGeneratorUrl = dotEnv['STUDYU_PROJECT_GENERATOR_URL'];
+  appUrl = dotEnv['STUDYU_APP_URL'];
   supabaseUrl = dotEnv['STUDYU_SUPABASE_URL']!;
   supabaseAnonKey = dotEnv['STUDYU_SUPABASE_PUBLIC_ANON_KEY']!;
   client = supabaseClient ?? SupabaseClient(supabaseUrl, supabaseAnonKey);
