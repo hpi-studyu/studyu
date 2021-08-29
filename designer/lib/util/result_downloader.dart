87bd9e5efabff5fd32bc238c6cc6cc9912d3ab78
Nils Strelow <strelow.nils@gmail.com>
2021-08-29 16:30:40 +0200

    lint: Fix unsafe use of href = ...

--- designer/lib/util/result_downloader.dart
+++ designer/lib/util/result_downloader.dart
@@ -43,15 +43,9 @@ Future<void> downloadFile(String contentString, String filename) async {
     final bytes = utf8.encode(contentString);
     final blob = html.Blob([bytes]);
     final url = html.Url.createObjectUrlFromBlob(blob);
-    final anchor = html.document.createElement('a') as html.AnchorElement
-      ..href = url
-      ..style.display = 'none'
-      ..download = filename;
-    html.document.body.children.add(anchor);
-
-    anchor.click();
-
-    html.document.body.children.remove(anchor);
+    html.AnchorElement(href: url)
+      ..download = url
+      ..click();
     html.Url.revokeObjectUrl(url);
   } else {
     final params = SaveFileDialogParams(data: Uint8List.fromList(contentString.codeUnits), fileName: filename);
