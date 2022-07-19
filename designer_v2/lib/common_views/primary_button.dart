cb020498e315083d9fc8e3ecc70270c4e720746f
janukobytsch <janusch.jacoby@gmail.com>
2022-07-19 16:05:43 +0200

    feat: add basic empty state to AsyncValueWidget

--- designer_v2/lib/common_views/primary_button.dart
+++ designer_v2/lib/common_views/primary_button.dart
@@ -3,22 +3,39 @@ import 'package:flutter/material.dart';
 class PrimaryButton extends StatelessWidget {
   /// The text displayed as the button label
   final String text;
+  /// The icon displayed to the left of the label
+  final IconData icon;
   /// If true, a loading indicator is displayed instead of the text
   final bool isLoading;
   /// Callback to be called when the button is pressd
   final VoidCallback? onPressed;
 
   const PrimaryButton({
-    Key? key, required this.text, this.isLoading = false, this.onPressed
+    required this.text,
+    this.icon = Icons.add,
+    this.isLoading = false,
+    this.onPressed,
+    Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    if (isLoading) {
+      return ElevatedButton(
         onPressed: onPressed,
         child: isLoading
             ? const CircularProgressIndicator()
             : Text(text, textAlign: TextAlign.center),
       );
+    }
+    return ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(
+          onPrimary: Theme.of(context).colorScheme.onPrimary,
+          primary: Theme.of(context).colorScheme.primary,
+        ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+      icon: Icon(icon),
+      label: Text(text),
+      onPressed: onPressed
+    );
   }
 }
