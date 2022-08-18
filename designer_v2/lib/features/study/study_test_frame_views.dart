72bacb3262a0e3896088744eb3a9e3a26b6cfb64
janukobytsch <janusch.jacoby@student.hpi.uni-potsdam.de>
2022-08-18 19:51:08 +0200

    feat: improve phone frame styling for test mode

--- designer_v2/lib/features/study/study_test_frame_views.dart
+++ designer_v2/lib/features/study/study_test_frame_views.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:studyu_designer_v2/common_views/empty_body.dart';
 
 class WebFrame extends StatelessWidget {
   final String previewSrc;
@@ -10,20 +11,98 @@ class WebFrame extends StatelessWidget {
     // todo make dynamic width should be half the size of height or height double the width
     // final height = MediaQuery.of(context).size.height;
     // final width = MediaQuery.of(context).size.width;
+    final theme = Theme.of(context);
+    return PhoneContainer(
+      innerContent: HtmlElementView(key: key, viewType: '$studyId$key'),
+      borderColor: theme.colorScheme.secondary.withOpacity(0.4),
+      innerContentBackgroundColor: theme.colorScheme.secondary.withOpacity(0.025),
+    );
+  }
+}
+
+class DisabledFrame extends StatelessWidget {
+  const DisabledFrame({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return PhoneContainer(
+      innerContent: const Center(
+        child: Opacity(
+          opacity: 0.3,
+          child: EmptyBody(
+            icon: Icons.visibility_off_rounded,
+            title: "",
+            description: "",
+          ),
+        ),
+      ),
+      borderColor: theme.colorScheme.secondary.withOpacity(0.25),
+      innerContentBackgroundColor: theme.colorScheme.secondary.withOpacity(0.03),
+    );
+  }
+}
+
+class PhoneContainer extends StatelessWidget {
+  const PhoneContainer({
+    required this.innerContent,
+    this.width = 300,
+    this.height = 600,
+    this.borderColor = Colors.black,
+    this.borderWidth = 6.0,
+    this.borderRadius = 25.0,
+    this.innerContentBackgroundColor = Colors.white,
+    Key? key
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+  final Color borderColor;
+  final double borderWidth;
+  final double borderRadius;
+
+  final Widget innerContent;
+  final Color? innerContentBackgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
-      height: 600,
-      width: 300,
+      height: height,
+      width: width,
       child: Stack(
         children: <Widget>[
-          ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(25)),
-            child: HtmlElementView(key: key, viewType: '$studyId$key'),
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+                child: Padding(
+                  padding: EdgeInsets.all(borderWidth),
+                  child: Stack(
+                    children: [
+                      Container(color: innerContentBackgroundColor),
+                      innerContent,
+                    ],
+                  ),
+                ),
+              ),
+              ClipRRect( // opaque border so that we can draw on top with other colors
+                borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+                      border: Border.all(width: borderWidth, color: Colors.white),
+                      shape: BoxShape.rectangle
+                  ),
+                ),
+              ),
+            ],
           ),
           Container(
             decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(25)),
-                border: Border.all(width: 4, color: Colors.blue),
-                shape: BoxShape.rectangle),
+              borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+              border: Border.all(width: borderWidth, color: borderColor),
+              shape: BoxShape.rectangle
+            ),
           ),
         ],
       ),
