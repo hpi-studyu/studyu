94128618e93c605dceba24cd7e1c8be7b0fdec6e
Johannes Vedder <johannes.vedder@student.hpi.de>
2023-02-19 16:17:20 +0100

    chore: enable test again

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:studyu_app/screens/app_onboarding/welcome.dart';
@@ -27,4 +27,3 @@ void main() {
     expect(find.text('Get started'), findsOneWidget);
   });
 }
-*/
