6a4da1594d7baed5d76f74464670ec411abc961c
Johannes Vedder <hallo@johannesvedder.de>
2023-04-24 19:51:15 +0200

    fix: upgrade deps to fix invalid refresh token bug

--- designer_v2/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ designer_v2/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -36,6 +36,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin shared_preferences_android, io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new com.aboutyou.dart_packages.sign_in_with_apple.SignInWithApplePlugin());
+    } catch(Exception e) {
+      Log.e(TAG, "Error registering plugin sign_in_with_apple, com.aboutyou.dart_packages.sign_in_with_apple.SignInWithApplePlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.urllauncher.UrlLauncherPlugin());
     } catch(Exception e) {
       Log.e(TAG, "Error registering plugin url_launcher_android, io.flutter.plugins.urllauncher.UrlLauncherPlugin", e);
