37d6e99baeab1bed2dfc78d77e78ad00f2a93f12
Jannis Baum <jannis.baum@gmail.com>
2023-06-10 17:36:16 +0200

    feat(#422): adjust contact data model

--- core/lib/src/models/contact.dart
+++ core/lib/src/models/contact.dart
@@ -11,10 +11,12 @@ class Contact {
   String email = '';
   String website = '';
   String phone = '';
+  String? additionalInfo;
 
   Contact();
 
-  factory Contact.fromJson(Map<String, dynamic> json) => _$ContactFromJson(json);
+  factory Contact.fromJson(Map<String, dynamic> json) =>
+      _$ContactFromJson(json);
 
   Map<String, dynamic> toJson() => _$ContactToJson(this);
 
