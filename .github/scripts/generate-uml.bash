831d7d82f223e102c6d725614521c1a19ae15c40
Jannis Baum <jannis.baum@gmail.com>
2023-07-12 17:07:08 +0200

    feat(#480): find uml dirty dirs

--- .github/scripts/generate-uml.bash
+++ .github/scripts/generate-uml.bash
@@ -3,8 +3,37 @@
 # repo root at ../.. from this file's directory
 root=$(realpath $(dirname $(realpath $0))/../..)
 
-# clear existing umls
 docs_dir="$root/docs/uml"
+# find latest commit that updated uml diagrams (or use initial commit)
+prev_update="$(git log -n 1 --pretty=format:%H -- $docs_dir)"
+[[ -z  "$prev_update" ]] && prev_update="$(git rev-list --max-parents=0 HEAD)"
+
+# associative array, keys will be all directories whose uml has to be
+# regenerated
+declare -A dirty
+# get all directories that have changed since prev_update
+for changed in $(git diff --name-only $prev_update \
+    | grep --extended-regexp '(flutter_common|core|designer_v2|app)/lib/.*\.dart' \
+    | xargs dirname \
+    | sort --unique \
+); do
+    # set parent as dirty list until we reach lib
+    while [[ -n $(grep --extended-regexp '[^/]*/lib/.+' <<< "$changed") ]]; do
+        dirty[$changed]=1
+        changed="$(dirname "$changed")"
+    done
+    # set lib dirty
+    dirty[$changed]=1
+done
+
+for d in "${!dirty[@]}"; do
+    echo "dirty: $d"
+done
+
+# check if dir still exists before regenerating uml
+
+exit 0
+
 rm -rf "$docs_dir"
 
 # temporary file for uml data
