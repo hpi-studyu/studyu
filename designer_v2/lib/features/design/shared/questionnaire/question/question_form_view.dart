65f96ed473ca8cae8a41caae7e8f02d8339335e0
sanemorak <sanem.orak@student.hpi.uni-potsdam.de>
2023-07-18 23:28:03 +0200

    image question type and related files are added

--- designer_v2/lib/features/design/shared/questionnaire/question/question_form_view.dart
+++ designer_v2/lib/features/design/shared/questionnaire/question/question_form_view.dart
@@ -9,6 +9,7 @@ import 'package:studyu_designer_v2/common_views/text_paragraph.dart';
 import 'package:studyu_designer_v2/features/design/shared/questionnaire/question/question_form_controller.dart';
 import 'package:studyu_designer_v2/features/design/shared/questionnaire/question/types/bool_question_form_view.dart';
 import 'package:studyu_designer_v2/features/design/shared/questionnaire/question/types/choice_question_form_view.dart';
+import 'package:studyu_designer_v2/features/design/shared/questionnaire/question/types/image_capturing_question_form_view.dart';
 import 'package:studyu_designer_v2/features/design/shared/questionnaire/question/types/question_type.dart';
 import 'package:studyu_designer_v2/features/design/shared/questionnaire/question/types/scale_question_form_view.dart';
 import 'package:studyu_designer_v2/features/forms/form_validation.dart';
@@ -18,19 +19,23 @@ import 'package:studyu_designer_v2/theme.dart';
 /// Wrapper that dispatches to the appropriate widget for the corresponding
 /// [SurveyQuestionType] as given by [formViewModel.questionType]
 class SurveyQuestionFormView extends ConsumerStatefulWidget {
-  const SurveyQuestionFormView({required this.formViewModel, this.isHtmlStyleable = true, super.key});
+  const SurveyQuestionFormView(
+      {required this.formViewModel, this.isHtmlStyleable = true, super.key});
 
   final QuestionFormViewModel formViewModel;
   final bool isHtmlStyleable;
 
   @override
-  ConsumerState<SurveyQuestionFormView> createState() => _SurveyQuestionFormViewState();
+  ConsumerState<SurveyQuestionFormView> createState() =>
+      _SurveyQuestionFormViewState();
 }
 
-class _SurveyQuestionFormViewState extends ConsumerState<SurveyQuestionFormView> {
+class _SurveyQuestionFormViewState
+    extends ConsumerState<SurveyQuestionFormView> {
   QuestionFormViewModel get formViewModel => widget.formViewModel;
 
-  late bool isQuestionHelpTextFieldVisible = formViewModel.questionInfoTextControl.value?.isNotEmpty ?? false;
+  late bool isQuestionHelpTextFieldVisible =
+      formViewModel.questionInfoTextControl.value?.isNotEmpty ?? false;
   bool isStylingInformationDismissed = true;
 
   onDismissedCallback() => setState(() {
@@ -39,14 +44,20 @@ class _SurveyQuestionFormViewState extends ConsumerState<SurveyQuestionFormView>
 
   WidgetBuilder get questionTypeBodyBuilder {
     final Map<SurveyQuestionType, WidgetBuilder> questionTypeWidgets = {
-      SurveyQuestionType.choice: (_) => ChoiceQuestionFormView(formViewModel: formViewModel),
-      SurveyQuestionType.bool: (_) => BoolQuestionFormView(formViewModel: formViewModel),
-      SurveyQuestionType.scale: (_) => ScaleQuestionFormView(formViewModel: formViewModel),
+      SurveyQuestionType.choice: (_) =>
+          ChoiceQuestionFormView(formViewModel: formViewModel),
+      SurveyQuestionType.bool: (_) =>
+          BoolQuestionFormView(formViewModel: formViewModel),
+      SurveyQuestionType.scale: (_) =>
+          ScaleQuestionFormView(formViewModel: formViewModel),
+      SurveyQuestionType.image: (_) =>
+          ImageCapturingQuestionFormView(formViewModel: formViewModel),
     };
     final questionType = formViewModel.questionType;
 
     if (!questionTypeWidgets.containsKey(questionType)) {
-      throw Exception("Failed to build widget for SurveyQuestionType $questionType because"
+      throw Exception(
+          "Failed to build widget for SurveyQuestionType $questionType because"
           "there is no registered WidgetBuilder");
     }
     final builder = questionTypeWidgets[questionType]!;
@@ -101,19 +112,25 @@ class _SurveyQuestionFormViewState extends ConsumerState<SurveyQuestionFormView>
               labelStyle: const TextStyle(fontWeight: FontWeight.bold),
               // TODO: extract custom dropdown component with theme + focus fix
               input: Theme(
-                data: theme.copyWith(inputDecorationTheme: ThemeConfig.dropdownInputDecorationTheme(theme)),
+                data: theme.copyWith(
+                    inputDecorationTheme:
+                        ThemeConfig.dropdownInputDecorationTheme(theme)),
                 child: ReactiveDropdownField<SurveyQuestionType>(
                   formControl: formViewModel.questionTypeControl,
                   items: formViewModel.questionTypeControlOptions.map((option) {
-                    final menuItemTheme = ThemeConfig.dropdownMenuItemTheme(theme);
-                    final iconTheme = menuItemTheme.iconTheme ?? theme.iconTheme;
+                    final menuItemTheme =
+                        ThemeConfig.dropdownMenuItemTheme(theme);
+                    final iconTheme =
+                        menuItemTheme.iconTheme ?? theme.iconTheme;
                     return DropdownMenuItem(
                       value: option.value,
                       child: Row(
                         children: [
                           (option.value.icon != null)
                               ? Icon(option.value.icon,
-                                  size: iconTheme.size, color: iconTheme.color, shadows: iconTheme.shadows)
+                                  size: iconTheme.size,
+                                  color: iconTheme.color,
+                                  shadows: iconTheme.shadows)
                               : const SizedBox.shrink(),
                           const SizedBox(width: 16.0),
                           Text(option.label)
@@ -158,7 +175,8 @@ class _SurveyQuestionFormViewState extends ConsumerState<SurveyQuestionFormView>
                       child: Hyperlink(
                         text: "styleable",
                         onClick: () => setState(() {
-                          isStylingInformationDismissed = !isStylingInformationDismissed;
+                          isStylingInformationDismissed =
+                              !isStylingInformationDismissed;
                         }),
                         visitedColor: null,
                       ),
@@ -184,7 +202,8 @@ class _SurveyQuestionFormViewState extends ConsumerState<SurveyQuestionFormView>
           ),
           input: ReactiveTextField(
             formControl: formViewModel.questionTextControl,
-            validationMessages: formViewModel.questionTextControl.validationMessages,
+            validationMessages:
+                formViewModel.questionTextControl.validationMessages,
             minLines: 3,
             maxLines: 3,
           ),
@@ -201,7 +220,8 @@ class _SurveyQuestionFormViewState extends ConsumerState<SurveyQuestionFormView>
           control: formViewModel.questionInfoTextControl,
           label: tr.form_field_question_help_text,
           labelHelpText: tr.form_field_question_help_text_tooltip,
-          labelBuilder: (context) => Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          labelBuilder: (context) =>
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
             Row(mainAxisAlignment: MainAxisAlignment.start, children: [
               FormLabel(
                 labelText: tr.form_field_question_help_text,
@@ -216,7 +236,8 @@ class _SurveyQuestionFormViewState extends ConsumerState<SurveyQuestionFormView>
                     child: Hyperlink(
                       text: "styleable",
                       onClick: () => setState(() {
-                        isStylingInformationDismissed = !isStylingInformationDismissed;
+                        isStylingInformationDismissed =
+                            !isStylingInformationDismissed;
                       }),
                       visitedColor: null,
                     ),
@@ -226,7 +247,8 @@ class _SurveyQuestionFormViewState extends ConsumerState<SurveyQuestionFormView>
           ]),
           input: ReactiveTextField(
             formControl: formViewModel.questionInfoTextControl,
-            validationMessages: formViewModel.questionInfoTextControl.validationMessages,
+            validationMessages:
+                formViewModel.questionInfoTextControl.validationMessages,
             minLines: 3,
             maxLines: 3,
             decoration: InputDecoration(
