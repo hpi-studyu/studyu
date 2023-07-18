db7ac58a9b61faa559137bfeea32e3a959d33ff8
sanemorak <sanem.orak@student.hpi.uni-potsdam.de>
2023-07-18 16:08:44 +0200

    Creation of Image Capturing Type

--- designer_v2/lib/features/design/shared/questionnaire/question/types/image_capturing_question_form_view.dart
+++ designer_v2/lib/features/design/shared/questionnaire/question/types/image_capturing_question_form_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+import 'package:studyu_designer_v2/common_views/standard_table.dart';
 import 'package:studyu_designer_v2/features/design/shared/questionnaire/question/question_form_controller.dart';
+import 'package:studyu_designer_v2/features/design/shared/questionnaire/question/types/choice_question_form_view.dart';
 
 class ImageCapturingQuestionFormView extends ConsumerWidget {
   const ImageCapturingQuestionFormView({required this.formViewModel, Key? key})
@@ -10,16 +13,33 @@ class ImageCapturingQuestionFormView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Provide directions for capturing image',
-            ),
+    return Column(
+      children: [
+        Opacity(
+          opacity: 0.5,
+          child: StandardTable<AbstractControl>(
+            items: formViewModel.answerOptionsControls,
+            columns: const [
+              StandardTableColumn(
+                label: '', // don't care (showTableHeader=false)
+                columnWidth: FixedColumnWidth(32.0),
+              ),
+              StandardTableColumn(
+                  label: '', // don't care (showTableHeader=false)
+                  columnWidth: FlexColumnWidth()),
+            ],
+            onSelectItem: (_) => {}, // no-op
+            buildCellsAt: (context, control, _, __) =>
+                buildChoiceOptionRow(context, control),
+            trailingActionsAt: (control, _) => [],
+            cellSpacing: 0.0,
+            rowSpacing: 8.0,
+            minRowHeight: null,
+            showTableHeader: false,
+            rowStyle: StandardTableStyle.plain,
+            disableRowInteractions: true,
+            trailingWidget: null,
+            trailingWidgetSpacing: 0,
           ),
         ),
       ],
