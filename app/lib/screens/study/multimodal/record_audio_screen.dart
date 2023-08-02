ce64952c950e3a8be06b9a714880abf12656de9b
Florian Papsdorf <florian.papsdorf@icloud.com>
2023-08-02 11:29:30 +0200

    Embedd audio recorder screen by dependencies

--- app/lib/screens/study/multimodal/record_audio_screen.dart
+++ app/lib/screens/study/multimodal/record_audio_screen.dart
@@ -1,11 +1,9 @@
-import 'package:encrypted_media_capturing/recorded_audio_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:record/record.dart';
-
-import '../persistent_storage_handler.dart';
+import 'package:studyu_core/core.dart';
 
 class RecordAudioScreen extends StatefulWidget {
   const RecordAudioScreen({super.key});
@@ -78,43 +76,32 @@ class RecordAudioScreenState extends State<RecordAudioScreen> {
     _isRecording ? _stopRecording() : _startRecording();
   }
 
-  Future<void> _jumpToRecordedAudioScreen() async {
-    await Navigator.of(context).push(
-        MaterialPageRoute(builder: (context) => const RecordedAudioScreen()));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Audio Recorder'),
-      ),
-      body: FutureBuilder<void>(
-          future: _initializeRecorderPermissionsFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Column(
-                children: [
-                  _isRecording
-                      ? LoadingAnimationWidget.staggeredDotsWave(
-                          color: const Color(0xFFEA3799), size: 200)
-                      : Container(),
-                  ElevatedButton(
-                    onPressed: _toggleRecording,
-                    child: Text(
-                        _isRecording ? 'Stop Recording' : 'Start Recording'),
-                  )
-                ],
-              );
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          }),
-      floatingActionButton: FloatingActionButton(
-        heroTag: "jumpToCapturedAudiosScreen",
-        onPressed: _jumpToRecordedAudioScreen,
-        child: const Icon(Icons.ac_unit),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Audio Recorder'),
+        ),
+        body: FutureBuilder<void>(
+            future: _initializeRecorderPermissionsFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Column(
+                  children: [
+                    _isRecording
+                        ? LoadingAnimationWidget.staggeredDotsWave(
+                            color: const Color(0xFFEA3799), size: 200)
+                        : Container(),
+                    ElevatedButton(
+                      onPressed: _toggleRecording,
+                      child: Text(
+                          _isRecording ? 'Stop Recording' : 'Start Recording'),
+                    )
+                  ],
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }));
   }
 }
