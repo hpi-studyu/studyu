8031b88e491152ed4c37b83b34ec2b8f5b2af051
Jannis Baum <jannis.baum@gmail.com>
2023-08-18 23:26:42 +0200

    feat: legend widgets

--- app/lib/screens/study/report/util/plot_utilities.dart
+++ app/lib/screens/study/report/util/plot_utilities.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:studyu_core/core.dart';
 
 Map<String, int> getInterventionPositions(List<Intervention> interventions) {
@@ -9,4 +10,61 @@ Map<String, int> getInterventionPositions(List<Intervention> interventions) {
   order[interventions.first.id] = 1;
   order[interventions.last.id] = 2;
   return order;
+}
+
+class LegendWidget extends StatelessWidget {
+  LegendWidget({
+    super.key,
+    required Legend legend,
+  }): name = legend.name, color = legend.color;
+
+  final String name;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+        ),
+        const SizedBox(width: 6),
+        Text(
+          name,
+          style: textTheme.bodyLarge,
+        ),
+      ],
+    );
+  }
+}
+
+class LegendsListWidget extends StatelessWidget {
+  const LegendsListWidget({
+    super.key,
+    required this.legends,
+  });
+  final List<Legend> legends;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 16,
+      children: legends
+          .map((legend) => LegendWidget(legend: legend))
+          .toList(),
+    );
+  }
+}
+
+class Legend {
+  Legend(this.name, this.color);
+  final String name;
+  final Color color;
 }
