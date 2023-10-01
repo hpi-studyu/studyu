fb07892010be337336a2a6373e9b8836a5ba99b8
Alisher Turubayev <alisher.turubayev@gmail.com>
2023-10-02 00:05:26 +0200

    feat: completed e2e tests + script

--- designer_v2/integration_test/app_test.dart
+++ designer_v2/integration_test/app_test.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol_finders/patrol_finders.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:studyu_designer_v2/features/app.dart';
 import 'package:studyu_flutter_common/studyu_flutter_common.dart';
@@ -9,13 +12,12 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:studyu_designer_v2/services/shared_prefs.dart';
 
-import 'robots/app_robot.dart';
-import 'robots/auth_robot.dart';
-import 'robots/studies_robot.dart';
+import 'robots/robots.dart';
 
 void main() {
   late SharedPreferences sharedPreferences;
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
   setUpAll(() async {
     await loadEnv();
@@ -23,67 +25,189 @@ void main() {
     // Turn off the # in the URLs on the web
     usePathUrlStrategy();
     sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.clear();
   });
 
   group('end-to-end tests', () {
-    testWidgets('Start and splash screen', (tester) async {
-      final appRobot = AppRobot(tester);
+    // NOTE:
+    //  the hydration of user is inconsistent and derails the tests
+    //  thus, we need to clear the sharedPreferences so that the
+    //  hydration does not happen
+    // When 'Remember me is added, this method can be removed - but
+    //  it could be a good practice to keep it so that the test instances
+    //  do not share any information in the app cache
+    setUp(() async => await sharedPreferences.clear()); // RM
 
-      await tester.pumpWidget(ProviderScope(overrides: [sharedPreferencesProvider.overrideWithValue(sharedPreferences)], child: const App()));
-      await tester.pump();
-      await appRobot.validateOnSplashScreen();
-      await tester.pump();
-      await appRobot.validateOnAuthScreen();
-    });
-    testWidgets('Sign up -> Sign out flow test', (tester) async {
-      final appRobot = AppRobot(tester);
-      final authRobot = AuthRobot(tester);
-      final studiesRobot = StudiesRobot(tester);
+    patrolWidgetTest('Sign up -> Sign out flow test', ($) async {
+      final appRobot = AppRobot($);
+      final authRobot = AuthRobot($);
+      final studiesRobot = StudiesRobot($);
 
-      await tester.pumpWidget(ProviderScope(overrides: [sharedPreferencesProvider.overrideWithValue(sharedPreferences)], child: const App()));
-      await tester.pump();
-      await appRobot.validateOnSplashScreen();
-      await tester.pump();
-      await appRobot.validateOnAuthScreen();
+      await $.pumpWidgetAndSettle(ProviderScope(overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ], child: const App()));
+      await appRobot.validateOnLoginScreen();
       await authRobot.navigateToSignUpScreen();
-      await tester.pump();
       await authRobot.enterEmail('test@email.com');
-      await tester.pump();
       await authRobot.enterPassword('password');
-      await tester.pump();
       await authRobot.enterPasswordConfirmation('password');
-      await tester.pump();
       await authRobot.tapTermsCheckbox();
-      await tester.pump();
       await authRobot.tapSignUpButton();
-      await tester.pump(const Duration(seconds: 3));
-      await studiesRobot.validateOnStudiesScreen();
-      await studiesRobot.tapSignOutButton();
-      await tester.pump();
-      await tester.pump(const Duration(seconds: 3));
-      await appRobot.validateOnAuthScreen();
-    });
-    testWidgets('Sign in -> Sign out flow test', (tester) async {
-      final appRobot = AppRobot(tester);
-      final authRobot = AuthRobot(tester);
-      final studiesRobot = StudiesRobot(tester);
 
-      await tester.pumpWidget(ProviderScope(overrides: [sharedPreferencesProvider.overrideWithValue(sharedPreferences)], child: const App()));
-      await tester.pump();
-      appRobot.validateOnSplashScreen();
-      await tester.pump();
-      appRobot.validateOnAuthScreen();
-      authRobot.enterEmail('test@email.com');
-      await tester.pump();
-      authRobot.enterPassword('password');
-      await tester.pump();
-      await authRobot.tapSignInButton();
-      await tester.pump(const Duration(seconds: 3));
       await studiesRobot.validateOnStudiesScreen();
       await studiesRobot.tapSignOutButton();
-      await tester.pump();
-      await tester.pump(const Duration(seconds: 3));
-      await appRobot.validateOnAuthScreen();
+
+      await appRobot.validateOnLoginScreen();
+    });
+
+    patrolWidgetTest('Sign in test', ($) async {
+      final appRobot = AppRobot($);
+      final authRobot = AuthRobot($);
+      final studiesRobot = StudiesRobot($);
+
+      await $.pumpWidgetAndSettle(ProviderScope(overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ], child: const App()));
+      await appRobot.validateOnLoginScreen();
+      await authRobot.enterEmail('test@email.com');
+      await authRobot.enterPassword('password');
+      await authRobot.tapSignInButton();
+
+      await studiesRobot.validateOnStudiesScreen();
+      await studiesRobot.tapSignOutButton(); // RM
+    });
+
+    /* NOTE: 
+        * remove skip value when remember me is implemented
+        * also remove all lines marked by RM
+       The hope is that the Session key will persist across test app instances
+       and the user will stay logged in 
+    */
+    patrolWidgetTest('Remember me function test', skip: true, ($) async {
+      final studiesRobot = StudiesRobot($);
+
+      await $.pumpWidgetAndSettle(ProviderScope(overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ], child: const App()));
+
+      studiesRobot.validateOnStudiesScreen();
+    });
+
+    patrolWidgetTest('Create a study/rename it test', ($) async {
+      final appRobot = AppRobot($); // RM
+      final authRobot = AuthRobot($); // RM
+      final studiesRobot = StudiesRobot($);
+      final studyDesignRobot = StudyDesignRobot($);
+      final studyInfoRobot = StudyInfoRobot($);
+
+      await $.pumpWidgetAndSettle(ProviderScope(overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ], child: const App()));
+      await appRobot.validateOnLoginScreen(); // RM
+      await authRobot.enterEmail('test@email.com'); // RM
+      await authRobot.enterPassword('password'); // RM
+      await authRobot.tapSignInButton(); // RM
+
+      await studiesRobot.validateNoStudiesFound();
+      await studiesRobot.tapNewStudyButton();
+
+      await studyDesignRobot.validateOnDesignScreen();
+      await studyInfoRobot.validateOnStudyInfoScreen();
+      await studyInfoRobot.enterStudyName('Test Study');
+
+      await studyDesignRobot.validateChangesSaved();
+      await studyDesignRobot.tapLeftDrawerButton();
+      await studyDesignRobot.tapMyStudiesButton();
+
+      await studiesRobot.validateOnStudiesScreen();
+      await studiesRobot.validateStudyDraftExists();
+
+      await studiesRobot.tapSignOutButton(); // RM
+    });
+
+    patrolWidgetTest('Publish a study test', ($) async {
+      final appRobot = AppRobot($); // RM
+      final authRobot = AuthRobot($); // RM
+      final studiesRobot = StudiesRobot($);
+      final studyDesignRobot = StudyDesignRobot($);
+      final studyInfoRobot = StudyInfoRobot($);
+      final studyInterventionsRobot = StudyInterventionsRobot($);
+      final studyMeasurementsRobot = StudyMeasurementsRobot($);
+
+      await $.pumpWidgetAndSettle(ProviderScope(overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ], child: const App()));
+      await appRobot.validateOnLoginScreen(); // RM
+      await authRobot.enterEmail('test@email.com'); // RM
+      await authRobot.enterPassword('password'); // RM
+      await authRobot.tapSignInButton(); // RM
+
+      await studiesRobot.validateOnStudiesScreen();
+      await studiesRobot.validateStudyDraftExists();
+      await studiesRobot.tapOnExistingStudy();
+
+      await studyDesignRobot.validateOnDesignScreen();
+
+      await studyInfoRobot.validateOnStudyInfoScreen();
+      await studyInfoRobot.enterStudyDescription('Test study description');
+      await studyInfoRobot.enterResponsibleOrg('Test Organization, Inc.');
+      await studyInfoRobot.enterInstitutionalReviewBoard('IRB of Test Organization, Inc.');
+      await studyInfoRobot.enterIRBProtocolNumber('456-112-324');
+      await studyInfoRobot.enterResponsiblePerson('Test First Name, Test Last Name');
+      await studyInfoRobot.enterWebsite('test-study.org');
+      await studyInfoRobot.enterContactEmail('test@email.com');
+      await studyInfoRobot.enterContactPhone('+491112221122');
+      await studyDesignRobot.validateChangesSaved();
+
+      await studyDesignRobot.navigateToParticipationScreen();
+      // NO-OP because exception in participation screening questionnare:
+      //
+      // TODO: add participation questionnare testing
+
+      await studyDesignRobot.navigateToInterventionsScreen();
+      // Repeat twice for two interventions
+      await studyInterventionsRobot.tapAddInterventionButton();
+      await studyInterventionsRobot.enterInterventionName('Test Intervention A');
+      await studyInterventionsRobot.enterInterventionDesciption('Test Intervention Description A');
+      await studyInterventionsRobot.tapAddInterventionTaskButton();
+      await studyInterventionsRobot.enterInterventionTaskName('Task 1A');
+      await studyInterventionsRobot.enterInterventionTaskDescription('Task 1A Description');
+      await studyInterventionsRobot.tapSaveInterventionTaskButton();
+      await studyInterventionsRobot.tapSaveInterventionButton();
+
+      await studyInterventionsRobot.tapAddInterventionButton();
+      await studyInterventionsRobot.enterInterventionName('Test Intervention B');
+      await studyInterventionsRobot.enterInterventionDesciption('Test Intervention Description B');
+      await studyInterventionsRobot.tapAddInterventionTaskButton();
+      await studyInterventionsRobot.enterInterventionTaskName('Task 1B');
+      await studyInterventionsRobot.enterInterventionTaskDescription('Task 1B Description');
+      await studyInterventionsRobot.tapSaveInterventionTaskButton();
+      await studyInterventionsRobot.tapSaveInterventionButton();
+
+      await studyDesignRobot.navigateToMeasurementsScreen();
+      await studyMeasurementsRobot.tapAddSurveyButton();
+      await studyMeasurementsRobot.enterSurveyName('Test Survey');
+      await studyMeasurementsRobot.enterSurveyIntroText('Test Intro');
+      await studyMeasurementsRobot.enterSurveyOutroText('Test Outro');
+      await studyMeasurementsRobot.tapAddSurveyQuestionButton();
+      await studyMeasurementsRobot.enterSurveyQuestionText('Test Question');
+      await studyMeasurementsRobot.enterSurveyQuestionOption1('Test Option 1');
+      await studyMeasurementsRobot.enterSurveyQuestionOption2('Test Option 2');
+      await studyMeasurementsRobot.tapSaveSurveyQuestionButton();
+      await studyMeasurementsRobot.tapSaveSurveyButton();
+
+      await studyDesignRobot.tapPublishButton();
+      await studyDesignRobot.tapConfirmPublishButton();
+      await studyDesignRobot.tapSkipForNowButton();
+      await studyDesignRobot.validateStudyPublished();
+
+      await studyDesignRobot.tapLeftDrawerButton();
+      await studyDesignRobot.tapMyStudiesButton();
+
+      await studiesRobot.validateOnStudiesScreen();
+      await studiesRobot.validateStudyPublished();
+
+      await studiesRobot.tapSignOutButton(); // RM
     });
   });
 }
--- designer_v2/integration_test/robots/auth_robot.dart
+++ designer_v2/integration_test/robots/auth_robot.dart
@@ -1,37 +1,38 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol_finders/patrol_finders.dart';
 import 'package:reactive_forms/reactive_forms.dart';
-import 'package:studyu_designer_v2/common_views/primary_button.dart';
+import 'package:studyu_designer_v2/localization/app_translation.dart';
 
 class AuthRobot {
-  const AuthRobot(this.tester);
+  const AuthRobot(this.$);
 
-  final WidgetTester tester;
+  final PatrolTester $;
 
   Future<void> enterEmail(String email) async {
-    await tester.enterText(find.widgetWithText(ReactiveTextField, 'Email'), email);
+    await $(ReactiveTextField).containing(tr.form_field_email).enterText(email);
   }
 
   Future<void> enterPassword(String password) async {
-    await tester.enterText(find.widgetWithText(ReactiveTextField, 'Password'), password);
+    await $(ReactiveTextField).containing(tr.form_field_password).enterText(password);
   }
 
-  Future<void> enterPasswordConfirmation(String password) async {
-    await tester.enterText(find.widgetWithText(ReactiveTextField, 'Confirm password'), password);
+  Future<void> enterPasswordConfirmation(String confirmPassword) async {
+    await $(ReactiveTextField).containing(tr.form_field_password_confirm).enterText(confirmPassword);
   }
 
   Future<void> tapTermsCheckbox() async {
-    await tester.tap(find.byType(ReactiveCheckbox));
+    await $(ReactiveCheckbox).tap();
   }
 
   Future<void> tapSignInButton() async {
-    await tester.tap(find.widgetWithText(PrimaryButton, 'Sign in'));
+    await $(tr.action_button_login).tap();
   }
 
   Future<void> tapSignUpButton() async {
-    await tester.tap(find.widgetWithText(PrimaryButton, 'Create account'));
+    await $(tr.action_button_signup).tap();
   }
 
   Future<void> navigateToSignUpScreen() async {
-    await tester.tap(find.text('Sign up'));
-  } 
+    await $(tr.link_signup).tap();
+  }
 }
