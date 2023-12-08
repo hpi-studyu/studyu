0a28df5a2bf39b18dfb642dcdeb3dd1863eba50a
Johannes Vedder <johannes.vedder@student.hpi.de>
2023-12-08 11:56:48 +0100

    refactor: move questions to subdir

--- app/lib/widgets/questionnaire/questions/scale_question_widget.dart
+++ app/lib/widgets/questionnaire/questions/scale_question_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:studyu_app/theme.dart';
 import 'package:studyu_core/core.dart';
 
-import 'custom_slider.dart';
+import '../custom_slider.dart';
 import 'question_widget.dart';
 
 class ScaleQuestionWidget extends QuestionWidget {
