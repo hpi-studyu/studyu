66093aacdb4d8221d4fcab2565cd392028e78763
Johannes Vedder <johannes.vedder@student.hpi.de>
2023-12-22 13:01:54 +0100

    style: increase logo size in app drawer

--- designer_v2/lib/features/app_drawer.dart
+++ designer_v2/lib/features/app_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:studyu_designer_v2/assets.dart';
 import 'package:studyu_designer_v2/common_views/icons.dart';
 import 'package:studyu_designer_v2/common_views/utils.dart';
 import 'package:studyu_designer_v2/localization/app_translation.dart';
@@ -213,8 +214,8 @@ class _AppDrawerState extends ConsumerState<AppDrawer> {
                 backgroundBlendMode: BlendMode.color,
               ),
               child: Image.asset(
-                'assets/images/icon_wide.png',
-                fit: BoxFit.scaleDown,
+                Assets.logoWide,
+                fit: BoxFit.cover,
               ),
             ),
           ),
