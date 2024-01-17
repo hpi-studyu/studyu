aa2fa9979073d1d7db48ed4125d58c0df434d47a
Halil Göcer <dev@hig.dev>
2024-01-17 16:03:13 +0100

    Revert "revert: "refactor: remove unused title""
    This reverts commit 96e6d5295d9d6fb3354e69bde81e41d27088879a.

--- designer_v2/lib/features/app_drawer.dart
+++ designer_v2/lib/features/app_drawer.dart
@@ -62,7 +62,6 @@ class GoRouterDrawerEntry extends DrawerEntry {
 
 class AppDrawer extends ConsumerStatefulWidget {
   const AppDrawer({
-    required this.title,
     this.width = 250,
     this.autoCloseDrawer = true,
     this.leftPaddingEntries = 28.0,
@@ -74,7 +73,6 @@ class AppDrawer extends ConsumerStatefulWidget {
     super.key,
   });
 
-  final String title;
   final int width;
   final bool autoCloseDrawer;
   final double leftPaddingEntries;
