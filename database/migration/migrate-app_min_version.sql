b456454bb60af218bfde07ad75ca008b67ae3db5
Johannes Vedder <hallo@johannesvedder.de>
2024-03-15 22:29:09 +0100

    fix: make migration usable

--- database/migration/migrate-app_min_version.sql
+++ database/migration/migrate-app_min_version.sql
@@ -1,2 +1,9 @@
 ALTER TABLE public.app_config
-ADD COLUMN app_min_version text NOT NULL;
+ADD COLUMN app_min_version text;
+
+UPDATE public.app_config
+SET app_min_version = '2.6.0'
+WHERE id = 'prod';
+
+ALTER TABLE public.app_config
+ALTER COLUMN app_min_version SET NOT NULL;
