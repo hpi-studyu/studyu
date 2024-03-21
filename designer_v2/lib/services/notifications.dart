24b85ac7f046c8902aced2dabf169c341f93d592
Halil Göcer <dev@hig.dev>
2024-03-21 20:10:41 +0100

    feat: close study

--- designer_v2/lib/services/notifications.dart
+++ designer_v2/lib/services/notifications.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:studyu_designer_v2/localization/app_translation.dart';
 import 'package:studyu_designer_v2/services/notification_types.dart';
 
@@ -18,6 +19,9 @@ class Notifications {
   static final studyDeleted = SnackbarIntent(
     message: tr.notification_study_deleted,
   );
+  static final studyClosed = SnackbarIntent(
+    message: tr.notification_study_closed,
+  );
   static final inviteCodeDeleted = SnackbarIntent(
     message: tr.notification_code_deleted,
   );
@@ -28,6 +32,11 @@ class Notifications {
       title: tr.dialog_study_delete_title,
       message: tr.dialog_study_delete_description,
       icon: Icons.delete_rounded,
+      actions: [NotificationDefaultActions.cancel]);
+  static final studyCloseConfirmation = AlertIntent(
+      title: tr.dialog_study_close_title,
+      message: tr.dialog_study_close_description,
+      icon: MdiIcons.accountLock,
       actions: [NotificationDefaultActions.cancel]);
 }
 
