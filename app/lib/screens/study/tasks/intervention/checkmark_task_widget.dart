9781094e4b7f3be37cbe30e30cb6fa1d155f20fb
Johannes Vedder <hallo@johannesvedder.de>
2024-06-12 11:04:49 +0200

    chore: adapt isRedundantClick

--- app/lib/screens/study/tasks/intervention/checkmark_task_widget.dart
+++ app/lib/screens/study/tasks/intervention/checkmark_task_widget.dart
@@ -18,33 +18,38 @@ class CheckmarkTaskWidget extends StatefulWidget {
 }
 
 class _CheckmarkTaskWidgetState extends State<CheckmarkTaskWidget> {
-  DateTime? loginClickTime;
+  DateTime _lastClickTime = DateTime.now();
   bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
       style: ButtonStyle(
-          backgroundColor: WidgetStateProperty.all<Color>(Colors.green),
-          textStyle: WidgetStateProperty.all<TextStyle>(
-              const TextStyle(color: Colors.white))),
+        backgroundColor: WidgetStateProperty.all<Color>(Colors.green),
+        textStyle: WidgetStateProperty.all<TextStyle>(
+          const TextStyle(color: Colors.white),
+        ),
+      ),
       onPressed: () async {
-        if (isRedundantClick(loginClickTime)) return;
+        if (isRedundantClick(_lastClickTime)) return;
         setState(() {
           _isLoading = true;
+          _lastClickTime = DateTime.now();
         });
         await handleTaskCompletion(context, (StudySubject? subject) async {
           try {
             await subject!.addResult<bool>(
-                taskId: widget.task!.id,
-                periodId: widget.completionPeriod!.id,
-                result: true);
+              taskId: widget.task!.id,
+              periodId: widget.completionPeriod!.id,
+              result: true,
+            );
           } on SocketException catch (_) {
             await subject!.addResult<bool>(
-                taskId: widget.task!.id,
-                periodId: widget.completionPeriod!.id,
-                result: true,
-                offline: true);
+              taskId: widget.task!.id,
+              periodId: widget.completionPeriod!.id,
+              result: true,
+              offline: true,
+            );
             rethrow;
           }
         });
