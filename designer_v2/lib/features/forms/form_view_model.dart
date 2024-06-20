c327da7aea27936917e569e764c528aea3961585
Johannes Vedder <hallo@johannesvedder.de>
2024-06-20 15:15:36 +0200

    Revert "fix: do not autosave on formarray"
    This reverts commit 8f1275496446633f6f57748c1425f2ca9ed0925b.

--- designer_v2/lib/features/forms/form_view_model.dart
+++ designer_v2/lib/features/forms/form_view_model.dart
@@ -412,9 +412,10 @@ abstract class FormViewModel<T> implements IFormGroupController {
       if (control is FormGroup) {
         continue; // don't listen to nested descendants
       } else if (control is FormArray) {
-        continue; // don't listen to nested descendants
-        // final collectionChanges = control.collectionChanges.listen(boundListener); // don't subscribe to control.valueChanges
-        // _immediateFormChildrenSubscriptions.add(collectionChanges);
+        final collectionChanges =
+            control.collectionChanges.listen(boundListener);
+        // don't subscribe to control.valueChanges
+        _immediateFormChildrenSubscriptions.add(collectionChanges);
       } else if (control is FormControl) {
         final valueChanges = control.valueChanges.listen(boundListener);
         _immediateFormChildrenSubscriptions.add(valueChanges);
