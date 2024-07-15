190db38ed963788017adbe5b34e98511ccc1301c
ibrahim <ibrahim.ozkn@outlook.com>
2024-07-15 12:50:53 +0200

    fix: added legend for participant activity tooltip

--- designer_v2/lib/features/monitor/participant/participant_legend.dart
+++ designer_v2/lib/features/monitor/participant/participant_legend.dart
@@ -17,46 +17,46 @@ class ParticipantLegend extends StatelessWidget {
           style: const TextStyle(fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 8.0),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        Row(
           children: [
-            Row(
-              children: [
-                _buildLegendItem(
-                  color: completeColor,
-                  tr.participant_details_color_legend_completed,
-                  tr.participant_details_completed_legend_tooltip,
-                ),
-                const SizedBox(width: 16.0),
-                _buildLegendItem(
-                  color: partiallyCompleteColor,
-                  tr.participant_details_color_legend_partially_completed,
-                  tr.participant_details_partially_completed_legend_tooltip,
-                  gradient: true,
-                ),
-                const SizedBox(width: 16.0),
-                _buildLegendItem(
-                  color: incompleteColor,
-                  tr.participant_details_color_legend_missed,
-                  tr.participant_details_incomplete_legend_tooltip,
-                ),
-              ],
+            _buildLegendItem(
+              color: completeColor,
+              tr.participant_details_color_legend_completed,
+              tr.participant_details_completed_legend_tooltip,
             ),
-            const SizedBox(height: 8.0),
-            Row(
-              children: [
-                _buildLegendItem(
-                  widget: const Text('\u{2705}'),
-                  tr.participant_details_color_legend_completed_task,
-                  tr.participant_details_color_legend_completed_task_tooltip,
-                ),
-                const SizedBox(width: 16.0),
-                _buildLegendItem(
-                  widget: const Text('\u{274C}'),
-                  tr.participant_details_color_legend_missed_task,
-                  tr.participant_details_color_legend_missed_task_tooltip,
-                ),
-              ],
+            const SizedBox(width: 16.0),
+            _buildLegendItem(
+              color: partiallyCompleteColor,
+              tr.participant_details_color_legend_partially_completed,
+              tr.participant_details_partially_completed_legend_tooltip,
+              gradient: true,
+            ),
+            const SizedBox(width: 16.0),
+            _buildLegendItem(
+              color: incompleteColor,
+              tr.participant_details_color_legend_missed,
+              tr.participant_details_incomplete_legend_tooltip,
+            ),
+          ],
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          tr.participant_details_color_tooltip_legend_title,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 8.0),
+        Row(
+          children: [
+            _buildLegendItem(
+              widget: const Text('\u{2705}'),
+              tr.participant_details_color_legend_completed_task,
+              tr.participant_details_color_legend_completed_task_tooltip,
+            ),
+            const SizedBox(width: 16.0),
+            _buildLegendItem(
+              widget: const Text('\u{274C}'),
+              tr.participant_details_color_legend_missed_task,
+              tr.participant_details_color_legend_missed_task_tooltip,
             ),
           ],
         ),
