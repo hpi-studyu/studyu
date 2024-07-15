9a58bd4f8663e07c6826e2e19ada4900f9c6ed44
ibrahim <ibrahim.ozkn@outlook.com>
2024-07-15 23:43:52 +0200

    feat: disable invite code creation for public studies

--- database/studyu-schema.sql
+++ database/studyu-schema.sql
@@ -890,11 +890,9 @@ AND (registry_published = true OR participation = 'open'::public.participation O
 -- Name: study_invite Editors can do everything with study invite codes; Type: POLICY; Schema: public; Owner: postgres
 --
 
-CREATE POLICY "Editors can do everything with study invite codes" ON public.study_invite USING (( SELECT public.can_edit(auth.uid(), study.*) AS can_edit
+CREATE POLICY "Editors can do everything with study invite codes if only participation is invite" ON public.study_invite USING (( SELECT public.can_edit(auth.uid(), study.*) AS can_edit
    FROM public.study
-  WHERE (study.id = study_invite.study_id)));
-
-
+  WHERE (study.id = study_invite.study_id AND study.participation = 'invite'::public.participation)));
 --
 -- Name: study_subject Users can do everything with their subjects; Type: POLICY; Schema: public; Owner: postgres
 --
