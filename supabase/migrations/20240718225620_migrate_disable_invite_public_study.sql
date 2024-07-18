9d1e2c26ce99b3b0f7c5686396b381eaf5de4ec2
ibrahim <ibrahim.ozkn@outlook.com>
2024-07-19 01:01:37 +0200

    chore: add migration for updating policy on study_invite table

--- supabase/migrations/20240718225620_migrate_disable_invite_public_study.sql
+++ supabase/migrations/20240718225620_migrate_disable_invite_public_study.sql
@@ -1,9 +1,10 @@
 -- Migrate policy
--- DROP POLICY "Editors can do everything with their studies"
+-- Drop existing policy
 
-DROP POLICY "Editors can do everything with study invite codes" ON public.study;
+DROP POLICY "Editors can do everything with study invite codes" ON public.study_invite;
 
--- CREATE POLICY where only the owner can edit the study if participation is invite
+-- Create new policy
+-- Editors can do everything with study invite codes if participation is invite
 CREATE POLICY "Editors can do everything with study invite codes if participation is invite" ON public.study_invite USING (( SELECT public.can_edit(auth.uid(), study.*) AS can_edit
    FROM public.study
   WHERE (study.id = study_invite.study_id AND study.participation = 'invite'::public.participation)));
