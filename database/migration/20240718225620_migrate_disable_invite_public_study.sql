7c312ebf0127c76c2be60c4d3a2d9a6a11d26b00
ibrahim <ibrahim.ozkn@outlook.com>
2024-07-22 12:25:13 +0200

    fix: grant read and delete permissions for invite codes to editors for public studies

--- database/migration/20240718225620_migrate_disable_invite_public_study.sql
+++ database/migration/20240718225620_migrate_disable_invite_public_study.sql
@@ -4,7 +4,46 @@
 DROP POLICY "Editors can do everything with study invite codes" ON public.study_invite;
 
 -- Create new policy
--- Editors can do everything with study invite codes if participation is invite
-CREATE POLICY "Editors can do everything with study invite codes if participation is invite" ON public.study_invite USING (( SELECT public.can_edit(auth.uid(), study.*) AS can_edit
+
+-- Name: study_invite Editors can manage their own invite-only study invite codes; Type: POLICY; Schema: public; Owner: postgres
+CREATE POLICY "Editors can manage their own invite-only study invite codes" ON public.study_invite
+USING (
+ (
+   SELECT public.can_edit(auth.uid(), study.*) AS can_edit
    FROM public.study
-  WHERE (study.id = study_invite.study_id AND study.participation = 'invite'::public.participation)));
+   WHERE study.id = study_invite.study_id
+   AND study.participation = 'invite'::public.participation
+ )
+);
+
+--
+-- Name: study_invite Editors can read their own open-study invite codes; Type: POLICY; Schema: public; Owner: postgres
+--
+
+CREATE POLICY "Editors can read their own open-study invite codes"
+ON public.study_invite
+FOR SELECT
+USING (
+ (
+  SELECT public.can_edit(auth.uid(), study.*) AS can_edit
+  FROM public.study
+  WHERE study.id = study_invite.study_id
+  AND study.participation = 'open'::public.participation
+ )
+);
+
+--
+-- Name: study_invite Editors can delete their own open-study invite codes; Type: POLICY; Schema: public; Owner: postgres
+--
+
+CREATE POLICY "Editors can delete their own open-study invite codes"
+ON public.study_invite
+FOR DELETE
+USING (
+  (
+    SELECT public.can_edit(auth.uid(), study.*) AS can_edit
+    FROM public.study
+    WHERE study.id = study_invite.study_id
+    AND study.participation = 'open'::public.participation
+  )
+);
