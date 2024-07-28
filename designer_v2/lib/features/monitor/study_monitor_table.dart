0dfd7571960149d9eab008f760567bb9603e6de4
Md Raju Ahmed <rajucse1705@gmail.com>
2024-07-29 01:04:46 +0200

    fix: sort arrow

--- designer_v2/lib/features/monitor/study_monitor_table.dart
+++ designer_v2/lib/features/monitor/study_monitor_table.dart
@@ -28,8 +28,8 @@ class StudyMonitorTable extends ConsumerWidget {
         (a, b) => a.participantId.compareTo(b.participantId),
         (a, b) =>
             a.inviteCode != null ? a.inviteCode!.compareTo(b.inviteCode!) : 0,
-        (a, b) => a.startedAt.compareTo(b.startedAt),
-        (a, b) => a.lastActivityAt.compareTo(b.lastActivityAt),
+        (a, b) => b.startedAt.compareTo(a.startedAt),
+        (a, b) => b.lastActivityAt.compareTo(a.lastActivityAt),
         (a, b) => a.currentDayOfStudy.compareTo(b.currentDayOfStudy),
         (a, b) => a.completedInterventions.compareTo(b.completedInterventions),
         (a, b) => a.completedSurveys.compareTo(b.completedSurveys),
