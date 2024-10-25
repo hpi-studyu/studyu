a6b2edced291daab405910bdd70a27f9aa39fa9d
Ibrahim Ozkan <51259479+ibrahimozkn@users.noreply.github.com>
2024-10-25 20:50:46 +0200

    fix: exclude notification and battery optimization debug options on web

--- app/lib/util/debug_screen.dart
+++ app/lib/util/debug_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -14,15 +15,19 @@ import 'package:url_launcher/url_launcher.dart';
 
 class DebugScreen {
   static Future<void> showDebugScreen(BuildContext context) async {
-    final studyNotifications = context.read<AppState>().studyNotifications;
+    final studyNotifications =
+        !kIsWeb ? context.read<AppState>().studyNotifications : null;
 
     final pendingNotifications = studyNotifications != null
         ? studyNotifications.flutterLocalNotificationsPlugin
             .pendingNotificationRequests()
         : Future.value([]);
 
-    final pendingNotificationsPlugin =
-        FlutterLocalNotificationsPlugin().pendingNotificationRequests();
+    final pendingNotificationsPlugin = !kIsWeb
+        ? FlutterLocalNotificationsPlugin().pendingNotificationRequests().then(
+              (value) => value.map((e) => e).toList(),
+            )
+        : Future.value(<PendingNotificationRequest>[]);
 
     bool? ignoreBatteryOptimizations;
     int? pendingNotificationRes;
@@ -31,12 +36,11 @@ class DebugScreen {
     final versionString =
         'Version: ${packageInfo.version} - ${packageInfo.buildNumber}';
     if (!context.mounted) return;
+
     showDialog(
       context: context,
       builder: (_) => AlertDialog(
-        title: const SelectableText(
-          'Debug Screen',
-        ),
+        title: const SelectableText('Debug Screen'),
         content: Column(
           children: [
             Text(versionString),
@@ -49,10 +53,11 @@ class DebugScreen {
                     queryParameters: {
                       'subject': '[StudyU] Debug Information',
                       'body': 'version: $versionString\n'
+                          'userId: ${context.read<AppState>().activeSubject!.userId} \n'
                           'ignoreBatteryOptimizations: ${ignoreBatteryOptimizations ?? 'null'}\n'
                           'pendingNotificationsNumber: ${pendingNotificationRes ?? 'null'}\n'
                           'pendingNotificationsPluginNumber: ${pendingNotificationsPluginRes ?? 'null'}\n'
-                          'scheduledNotificationsDebug: ${StudyNotifications.scheduledNotificationsDebug}',
+                          'scheduledNotificationsDebug: ${StudyNotifications.scheduledNotificationsDebug} \n',
                     },
                   );
                   launchUrl(emailLaunchUri);
@@ -63,7 +68,7 @@ class DebugScreen {
               ),
               child: const Text('Send debug information via email'),
             ),
-            if (context.read<AppState>().studyNotifications != null)
+            if (!kIsWeb && context.read<AppState>().studyNotifications != null)
               ElevatedButton(
                 onPressed: () {
                   testNotification(context);
@@ -73,109 +78,115 @@ class DebugScreen {
                 ),
                 child: const Text('Receive test notification'),
               ),
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (_) => AlertDialog(
-                    title: const Text('Reset App?'),
-                    content: const Text(
-                      'This will delete all data and reset the app.',
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Cancel'),
+            if (!kIsWeb)
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) => AlertDialog(
+                      title: const Text('Reset App?'),
+                      content: const Text(
+                        'This will delete all data and reset the app.',
                       ),
-                      TextButton(
-                        onPressed: () async {
-                          await _deleteCacheDir();
-                          await _deleteAppDir();
-                          await SecureStorage.deleteAll();
-                          if (context.mounted) {
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
                             Navigator.of(context).pop();
-                            Navigator.of(context).pop();
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text(
-                                  'App reset successfully! Please restart the app.',
+                          },
+                          child: const Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () async {
+                            await _deleteCacheDir();
+                            await _deleteAppDir();
+                            await SecureStorage.deleteAll();
+                            if (context.mounted) {
+                              Navigator.of(context).pop();
+                              Navigator.of(context).pop();
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text(
+                                    'App reset successfully! Please restart the app.',
+                                  ),
                                 ),
-                              ),
-                            );
-                            await Future.delayed(const Duration(seconds: 1));
-                            await SystemChannels.platform
-                                .invokeMethod('SystemNavigator.pop');
-                          }
-                        },
-                        child: const Text('Reset'),
-                      ),
-                    ],
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.error,
+                              );
+                              await Future.delayed(const Duration(seconds: 1));
+                              await SystemChannels.platform
+                                  .invokeMethod('SystemNavigator.pop');
+                            }
+                          },
+                          child: const Text('Reset'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.error,
+                ),
+                child: const Text('Reset App'),
               ),
-              child: const Text('Reset App'),
-            ),
-            FutureBuilder<bool>(
-              future: receivePermission(),
-              builder: (
-                context,
-                AsyncSnapshot<bool> snapshot,
-              ) {
-                if (snapshot.hasData) {
-                  final String data =
-                      "ignoreBatteryOptimizations: ${snapshot.data}";
-                  ignoreBatteryOptimizations = snapshot.data;
-                  return Text(data);
-                } else {
-                  return const CircularProgressIndicator();
-                }
-              },
-            ),
-            FutureBuilder<List>(
-              future: pendingNotifications,
-              builder: (
-                context,
-                AsyncSnapshot<List> snapshot,
-              ) {
-                if (snapshot.hasData) {
-                  pendingNotificationRes = snapshot.data!.length;
-                  return Text(
-                    'Number of Pending Notifications: $pendingNotificationRes',
-                  );
-                } else if (snapshot.hasError) {
-                  return Text(
-                    'Pending Notifications: Error: ${snapshot.error}',
-                  );
-                } else {
-                  return const CircularProgressIndicator();
-                }
-              },
-            ),
-            FutureBuilder<List<PendingNotificationRequest>>(
-              future: pendingNotificationsPlugin,
-              builder: (
-                context,
-                AsyncSnapshot<List<PendingNotificationRequest>> snapshot,
-              ) {
-                if (snapshot.hasData) {
-                  pendingNotificationsPluginRes = snapshot.data!.length;
-                  return Text(
-                    'Local Notifications Plugin Number of Pending Notifications: $pendingNotificationsPluginRes',
-                  );
-                } else if (snapshot.hasError) {
-                  return Text(
-                    'Pending Notifications Plugin: Error: ${snapshot.error}',
-                  );
-                } else {
-                  return const CircularProgressIndicator();
-                }
-              },
-            ),
+            if (!kIsWeb)
+              FutureBuilder<bool>(
+                future: receivePermission(),
+                builder: (
+                  context,
+                  AsyncSnapshot<bool> snapshot,
+                ) {
+                  if (snapshot.hasData) {
+                    final String data =
+                        "ignoreBatteryOptimizations: ${snapshot.data}";
+                    ignoreBatteryOptimizations = snapshot.data;
+                    return Text(data);
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+            if (!kIsWeb)
+              FutureBuilder<List>(
+                future: pendingNotifications,
+                builder: (
+                  context,
+                  AsyncSnapshot<List> snapshot,
+                ) {
+                  if (snapshot.hasData) {
+                    pendingNotificationRes = snapshot.data!.length;
+                    return Text(
+                      'Number of Pending Notifications: $pendingNotificationRes',
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text(
+                      'Pending Notifications: Error: ${snapshot.error}',
+                    );
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+            if (!kIsWeb)
+              FutureBuilder<List<PendingNotificationRequest>>(
+                future: pendingNotificationsPlugin,
+                builder: (
+                  context,
+                  AsyncSnapshot<List<PendingNotificationRequest>> snapshot,
+                ) {
+                  if (snapshot.hasData) {
+                    pendingNotificationsPluginRes = snapshot.data!.length;
+                    return Text(
+                      'Local Notifications Plugin Number of Pending Notifications: $pendingNotificationsPluginRes',
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text(
+                      'Pending Notifications Plugin: Error: ${snapshot.error}',
+                    );
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+            Text('userId: ${context.read<AppState>().activeSubject!.userId}'),
+            SizedBox(height: 10),
             const Text("Scheduled Notifications:"),
             SelectableText(
               StudyNotifications.scheduledNotificationsDebug ?? 'No data',
@@ -188,6 +199,8 @@ class DebugScreen {
   }
 
   static Future<void> testNotification(BuildContext context) async {
+    if (kIsWeb) return;
+
     final studyNotifications = context.read<AppState>().studyNotifications;
     if (studyNotifications == null) {
       Navigator.of(context).pop();
@@ -201,22 +214,20 @@ class DebugScreen {
       return;
     }
     await studyNotifications.flutterLocalNotificationsPlugin.show(
-      /*******************/
       99,
       'StudyU Test Notification',
       'This notification confirms that you receive StudyU notifications',
-      /*******************/
       notificationDetails,
     );
   }
 
   static Future<bool> receivePermission() async {
+    if (kIsWeb) return false;
     return await Permission.ignoreBatteryOptimizations.request().isGranted;
   }
 
   static Future<void> _deleteCacheDir() async {
     final cacheDir = await getTemporaryDirectory();
-
     if (cacheDir.existsSync()) {
       cacheDir.deleteSync(recursive: true);
     }
