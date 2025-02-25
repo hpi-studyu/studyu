a8da0d25140d4c78f69b53b3554ee682a2d0b902
ibrahimozkan <ibrahim.ozkn@outlook.com>
2025-02-25 22:40:15 +0200

    feat: update Fitbit redirect URL and callback scheme

--- app/lib/constants.dart
+++ app/lib/constants.dart
@@ -1,5 +1,5 @@
 const String playStoreUrl =
     'https://play.google.com/store/apps/details?id=health.studyu.app';
 const String appstoreUrl = 'https://itunes.apple.com/app/id1571991198';
-const String fitbitRedirectUrl = 'example://fitbit/auth';
-const String fitbitCallbackScheme = 'example';
+const String fitbitRedirectUrl = 'studyu://fitbit/auth';
+const String fitbitCallbackScheme = 'studyu';
