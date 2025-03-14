9edccf2005becdc5bdd9441cbe80d3d17fb82885
Johannes Vedder <hallo@johannesvedder.de>
2025-03-14 11:17:48 +0100

    format: localization files

--- app/lib/l10n/app_localizations_en.dart
+++ app/lib/l10n/app_localizations_en.dart
@@ -15,40 +15,31 @@ class AppLocalizationsEn extends AppLocalizations {
   String get what_is_studyu => 'What is StudyU?';
 
   @override
-  String get description_part1 =>
-      'Imagine reading the sentence: \"Eating after 6 pm decreases sleep quality\"';
+  String get description_part1 => 'Imagine reading the sentence: \"Eating after 6 pm decreases sleep quality\"';
 
   @override
-  String get description_part2 =>
-      'Now you might think something like: Well... good to know but is that affecting everyone and also ME?';
+  String get description_part2 => 'Now you might think something like: Well... good to know but is that affecting everyone and also ME?';
 
   @override
-  String get description_part3 =>
-      'The problem is: you did not take part in the study yourself, so we simply cannot answer that question. A traditional study can only answer whether it is more LIKELY that your sleep quality is affected. You would therefore have to test the effect of eating late on YOUR sleep.';
+  String get description_part3 => 'The problem is: you did not take part in the study yourself, so we simply cannot answer that question. A traditional study can only answer whether it is more LIKELY that your sleep quality is affected. You would therefore have to test the effect of eating late on YOUR sleep.';
 
   @override
-  String get description_part4 =>
-      'This means that you would have to do your own personal study, in which you would have phases of eating late and phases of abstaining from eating late. You would regularly assess your sleep quality and in the end come to a result that could finally answer the question of whether eating late decreases your sleep quality or not. Giving you a reliable answer to such questions is the goal of StudyU.';
+  String get description_part4 => 'This means that you would have to do your own personal study, in which you would have phases of eating late and phases of abstaining from eating late. You would regularly assess your sleep quality and in the end come to a result that could finally answer the question of whether eating late decreases your sleep quality or not. Giving you a reliable answer to such questions is the goal of StudyU.';
 
   @override
-  String get description_part5 =>
-      'StudyU offers the possibility to enroll to N-of-1 studies designed by experts. N-of-1 means that the number of people in the trials, which is usually indicated as N, is 1. And just like traditional trials, N-of-1 trials need a clearly defined plan (a so-called study protocol).';
+  String get description_part5 => 'StudyU offers the possibility to enroll to N-of-1 studies designed by experts. N-of-1 means that the number of people in the trials, which is usually indicated as N, is 1. And just like traditional trials, N-of-1 trials need a clearly defined plan (a so-called study protocol).';
 
   @override
-  String get description_part6 =>
-      'And because good study protocols are not easy to make, we have developed this App. Here you can choose between different N-of-1 studies, according to YOUR personal interest, and you will automatically receive a plan developed by experts that will give you a reliable result.';
+  String get description_part6 => 'And because good study protocols are not easy to make, we have developed this App. Here you can choose between different N-of-1 studies, according to YOUR personal interest, and you will automatically receive a plan developed by experts that will give you a reliable result.';
 
   @override
-  String get description_part7 =>
-      'Once you have chosen one of our studies we will make sure that your health status allows participation. Afterwards you can enroll as a participant and adapt the study plan to your everyday life. Tasks (e.g. eating late and rating your tiredness) have to be done on a regular basis (e.g. once per day). Once you have reached the minimum study duration (usually just a few weeks) you will be able to unlock results for free.';
+  String get description_part7 => 'Once you have chosen one of our studies we will make sure that your health status allows participation. Afterwards you can enroll as a participant and adapt the study plan to your everyday life. Tasks (e.g. eating late and rating your tiredness) have to be done on a regular basis (e.g. once per day). Once you have reached the minimum study duration (usually just a few weeks) you will be able to unlock results for free.';
 
   @override
-  String get description_part8 =>
-      'But bear in mind that results are more reliable the longer you take actively part in the study. And in order to prevent systematic error you cannot go on with the study after unlocking results. Therefore, with the help of a progress bar we will indicate you how many tasks are still needed for the minimum and how much you could improve your results with going on for some more weeks.';
+  String get description_part8 => 'But bear in mind that results are more reliable the longer you take actively part in the study. And in order to prevent systematic error you cannot go on with the study after unlocking results. Therefore, with the help of a progress bar we will indicate you how many tasks are still needed for the minimum and how much you could improve your results with going on for some more weeks.';
 
   @override
-  String get description_part9 =>
-      'But enough from our side, now it\'s time for StudyU!';
+  String get description_part9 => 'But enough from our side, now it\'s time for StudyU!';
 
   @override
   String get get_started => 'Get started';
@@ -60,33 +51,28 @@ class AppLocalizationsEn extends AppLocalizations {
   String get study_selection_description => 'Please select a study.';
 
   @override
-  String get study_selection_single =>
-      'You can only participate in one study at a time.';
+  String get study_selection_single => 'You can only participate in one study at a time.';
 
   @override
   String get study_selection_single_why => 'Why?';
 
   @override
-  String get study_selection_single_reason =>
-      'If you were to participate in multiple studies at a time, the interventions of these studies might interfere with one another and alter the results.';
+  String get study_selection_single_reason => 'If you were to participate in multiple studies at a time, the interventions of these studies might interfere with one another and alter the results.';
 
   @override
   String get study_selection_unsupported_title => 'Outdated app version';
 
   @override
-  String get study_selection_unsupported =>
-      'The study you are trying to join is not compatible with your app version. Please update the app to the latest version.';
+  String get study_selection_unsupported => 'The study you are trying to join is not compatible with your app version. Please update the app to the latest version.';
 
   @override
   String get study_selection_closed_title => 'Study closed';
 
   @override
-  String get study_selection_closed =>
-      'This study is currently closed for new participants.';
+  String get study_selection_closed => 'This study is currently closed for new participants.';
 
   @override
-  String get study_selection_hidden_studies =>
-      'Some studies couldn\'t be shown, because your app version is outdated. Please update your app to see all available studies.';
+  String get study_selection_hidden_studies => 'Some studies couldn\'t be shown, because your app version is outdated. Please update your app to see all available studies.';
 
   @override
   String get study_overview_title => 'Overview';
@@ -95,19 +81,16 @@ class AppLocalizationsEn extends AppLocalizations {
   String get eligibility_questionnaire_title => 'Questionnaire';
 
   @override
-  String get please_answer_eligibility =>
-      'Please answer a few questions to make sure that you can safely participate in this study.';
+  String get please_answer_eligibility => 'Please answer a few questions to make sure that you can safely participate in this study.';
 
   @override
   String get intervention_selection_title => 'Interventions';
 
   @override
-  String get please_select_interventions =>
-      'Please select two interventions to apply during the study.';
+  String get please_select_interventions => 'Please select two interventions to apply during the study.';
 
   @override
-  String get please_select_interventions_description =>
-      'The effects of these two interventions will be measured and compared during the study.';
+  String get please_select_interventions_description => 'The effects of these two interventions will be measured and compared during the study.';
 
   @override
   String get no_interventions_available => 'No interventions available.';
@@ -137,8 +120,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get tasks_daily => 'Tasks:';
 
   @override
-  String get baseline_description =>
-      'The baseline is a phase within a study in which the initial state is measured to allow later comparisons. During the baseline phase you should behave as usual, no study-specific interventions are carried out yet.';
+  String get baseline_description => 'The baseline is a phase within a study in which the initial state is measured to allow later comparisons. During the baseline phase you should behave as usual, no study-specific interventions are carried out yet.';
 
   @override
   String get baseline => 'Baseline';
@@ -165,12 +147,10 @@ class AppLocalizationsEn extends AppLocalizations {
   String get soft_delete_desc => 'You will lose your progress in ';
 
   @override
-  String get soft_delete_desc_2 =>
-      ' and won\'t be able to recover it. Previously completed studies will not be deleted.\nYour anonymized data up to this point may still be used for research purposes.';
+  String get soft_delete_desc_2 => ' and won\'t be able to recover it. Previously completed studies will not be deleted.\nYour anonymized data up to this point may still be used for research purposes.';
 
   @override
-  String get hard_delete_desc =>
-      'You are about to delete all data from your device and our servers. You will not be able to restore your data.\nYour anonymized data will not be available for research purposes anymore.';
+  String get hard_delete_desc => 'You are about to delete all data from your device and our servers. You will not be able to restore your data.\nYour anonymized data will not be available for research purposes anymore.';
 
   @override
   String get your_journey => 'Your Journey';
@@ -203,26 +183,22 @@ class AppLocalizationsEn extends AppLocalizations {
   String get finished => 'Finished';
 
   @override
-  String get how_would_you_rate_your_pain_today =>
-      'How would you rate your pain today? (0 = no pain, 10 = extreme pain)';
+  String get how_would_you_rate_your_pain_today => 'How would you rate your pain today? (0 = no pain, 10 = extreme pain)';
 
   @override
   String get thank_you_for_your_input => 'Thank you for your input';
 
   @override
-  String get please_give_consent =>
-      'Please give your consent to participate in this study. You are required to read all boxes by clicking on them.';
+  String get please_give_consent => 'Please give your consent to participate in this study. You are required to read all boxes by clicking on them.';
 
   @override
   String get please_give_consent_why => 'Why?';
 
   @override
-  String get please_give_consent_reason =>
-      'Studies need to request specific consent from participants, for reasons of safety and data privacy. Hence, you must explicitly consent to participate in each study.';
+  String get please_give_consent_reason => 'Studies need to request specific consent from participants, for reasons of safety and data privacy. Hence, you must explicitly consent to participate in each study.';
 
   @override
-  String get user_did_not_give_consent =>
-      'You did not give your consent. To participate you need to give consent.';
+  String get user_did_not_give_consent => 'You did not give your consent. To participate you need to give consent.';
 
   @override
   String get setting_up_study => 'Setting up your study...';
@@ -321,8 +297,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get audio_recorded => 'Audio recorded';
 
   @override
-  String get multimodal_not_supported =>
-      'Multimodal Trials are currently not supported to run in a web browser. Please use the StudyU App for Android or iOS.';
+  String get multimodal_not_supported => 'Multimodal Trials are currently not supported to run in a web browser. Please use the StudyU App for Android or iOS.';
 
   @override
   String get camera_access_denied => 'Camera access denied';
@@ -361,8 +336,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get allow_analytics => 'Allow app analytics';
 
   @override
-  String get allow_analytics_desc =>
-      'All collected data is used only to improve app performance and never for tracking purposes. You can read more about this in our data privacy.';
+  String get allow_analytics_desc => 'All collected data is used only to improve app performance and never for tracking purposes. You can read more about this in our data privacy.';
 
   @override
   String get video_test => 'This is a video test';
@@ -392,8 +366,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get terms_read => 'Read Terms of Use';
 
   @override
-  String get terms_content =>
-      'The terms of use give an overview on the purpose and use of the StudyU app. In case you have any questions please reach out to us via the contact information in the legal notice.';
+  String get terms_content => 'The terms of use give an overview on the purpose and use of the StudyU app. In case you have any questions please reach out to us via the contact information in the legal notice.';
 
   @override
   String get terms_agree => 'I have read and agree to the terms of use';
@@ -405,8 +378,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get privacy_read => 'Read Privacy Policy';
 
   @override
-  String get privacy_content =>
-      'The privacy policy describes which data is stored, why, when, where, access rights, and which rights you have. In case you have any questions please reach out to us via the contact information in the legal notice.';
+  String get privacy_content => 'The privacy policy describes which data is stored, why, when, where, access rights, and which rights you have. In case you have any questions please reach out to us via the contact information in the legal notice.';
 
   @override
   String get privacy_agree => 'I have read and agree to the privacy policy';
@@ -436,8 +408,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get save_not_supported => 'Error';
 
   @override
-  String get save_not_supported_description =>
-      'Downloading files is currently not supported in the web version.';
+  String get save_not_supported_description => 'Downloading files is currently not supported in the web version.';
 
   @override
   String get eligible_no => 'You are not eligible for this study';
@@ -446,8 +417,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get eligible_yes => 'You are eligible for this study';
 
   @override
-  String get eligible_mistake =>
-      'If you made a mistake, you can still change your answers';
+  String get eligible_mistake => 'If you made a mistake, you can still change your answers';
 
   @override
   String get eligible_back => 'Back to study selection';
@@ -462,8 +432,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get report_primary_result => 'Primary Result';
 
   @override
-  String get report_disclaimer =>
-      'This report is only valid if you entered all information correctly.';
+  String get report_disclaimer => 'This report is only valid if you entered all information correctly.';
 
   @override
   String get performance => 'Performance';
@@ -478,12 +447,10 @@ class AppLocalizationsEn extends AppLocalizations {
   String get performance_overview_observations => 'Observations';
 
   @override
-  String get report_outcome_inconclusive =>
-      'The results are inconclusive. There does not seem to be a statistically significant difference between the interventions.';
+  String get report_outcome_inconclusive => 'The results are inconclusive. There does not seem to be a statistically significant difference between the interventions.';
 
   @override
-  String get report_outcome_neither =>
-      'Both interventions seem to have a negative effect on the outcome for you.';
+  String get report_outcome_neither => 'Both interventions seem to have a negative effect on the outcome for you.';
 
   @override
   String report_outcome_one(Object intervention) {
@@ -494,26 +461,22 @@ class AppLocalizationsEn extends AppLocalizations {
   String get report_axis_phase => 'Phase';
 
   @override
-  String get study_not_started =>
-      'Your study has not started yet. Please check back tomorrow!';
+  String get study_not_started => 'Your study has not started yet. Please check back tomorrow!';
 
   @override
-  String get completed_study =>
-      'You completed your last study. Look at past reports or start a new study.';
+  String get completed_study => 'You completed your last study. Look at past reports or start a new study.';
 
   @override
   String get app_support => 'App support';
 
   @override
-  String get app_support_text =>
-      'Contact for problems or questions with the app';
+  String get app_support_text => 'Contact for problems or questions with the app';
 
   @override
   String get study_support => 'Study support';
 
   @override
-  String get study_support_text =>
-      'Contact for problems or questions with the study';
+  String get study_support_text => 'Contact for problems or questions with the study';
 
   @override
   String get organization => 'Organization';
@@ -547,8 +510,7 @@ class AppLocalizationsEn extends AppLocalizations {
   }
 
   @override
-  String get free_text_alphanumeric_error =>
-      'Please enter only alphanumeric characters';
+  String get free_text_alphanumeric_error => 'Please enter only alphanumeric characters';
 
   @override
   String get free_text_numeric_error => 'Please enter only numeric characters';
@@ -559,8 +521,7 @@ class AppLocalizationsEn extends AppLocalizations {
   }
 
   @override
-  String get app_outdated_message =>
-      'A new version of the StudyU App is available. Please update to get the latest features and improvements. Thank you for your support!';
+  String get app_outdated_message => 'A new version of the StudyU App is available. Please update to get the latest features and improvements. Thank you for your support!';
 
   @override
   String get update_now => 'Update now';
@@ -572,8 +533,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get fitbit_data_synced => 'Fitbit data synced successfully';
 
   @override
-  String get fitbit_data_not_synced =>
-      'Fitbit data could not be synced. Please be sure that you have synced your Fitbit data with the Fitbit app.';
+  String get fitbit_data_not_synced => 'Fitbit data could not be synced. Please be sure that you have synced your Fitbit data with the Fitbit app.';
 
   @override
   String error_syncing_fitbit_data(String error) {
