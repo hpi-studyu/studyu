26b3aedcf7738d7aeac2fadbac10dc8c9dd7fcdf
Johannes Vedder <hallo@johannesvedder.de>
2025-05-08 16:28:48 +0200

    fix: add string_extensions.dart

---
+++ app/lib/util/string_extensions.dart
@@ -0,0 +1,9 @@
+extension StringExtensions on String {
+  String toPascalCase() {
+    return split(RegExp(r'[\s_]+'))
+        .map((word) => word.isNotEmpty
+            ? word[0].toUpperCase() + word.substring(1).toLowerCase()
+            : '')
+        .join();
+  }
+}
