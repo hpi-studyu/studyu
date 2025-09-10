8adc158a55f9ea5c09ce8ac80d1601a559b3e0cb
ibrahimozkan <ibrahim.ozkn@outlook.com>
2025-09-10 12:03:57 +0200

    fix: prevent reordering of questions in readonly forms

--- designer_v2/lib/features/design/measurements/survey/survey_form_view.dart
+++ designer_v2/lib/features/design/measurements/survey/survey_form_view.dart
@@ -214,7 +214,7 @@ class _MeasurementSurveyFormViewState
                       ],
                     );
                   },
-                  reorderable: widget.formViewModel.questionModels.length > 1,
+                  reorderable: widget.formViewModel.questionModels.length > 1 && !widget.formViewModel.isReadonly,
                   onReorder: (oldIndex, newIndex) {
                     if (newIndex > oldIndex) {
                       newIndex -= 1;
