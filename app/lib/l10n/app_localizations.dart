5aef9ef3e01ba9e111dd4432bfe68c464fc8090e
johannesvedder <104295301+johannesvedder@users.noreply.github.com>
2026-07-11 12:17:20 +0000

    chore: Apply static analysis changes

--- app/lib/l10n/app_localizations.dart
+++ app/lib/l10n/app_localizations.dart
@@ -329,7 +329,7 @@ abstract class AppLocalizations {
   /// No description provided for @study_selection_hidden_studies.
   ///
   /// In en, this message translates to:
-  /// **'Some studies could not be displayed. This can happen when your app version is outdated. Please update the app to see all available studies, or join one of the studies shown below.'**
+  /// **'Some studies couldn\'t be shown, because your app version is outdated. Please update your app to see all available studies.'**
   String get study_selection_hidden_studies;
 
   /// No description provided for @study_overview_title.
