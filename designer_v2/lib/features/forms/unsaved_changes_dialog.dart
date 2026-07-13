8b9a4d6d59b28524fe6de5fd4850b8eb9bca9014
Johannes Vedder <hallo@johannesvedder.de>
2026-07-13 14:16:59 +0200

    fix(designer): intercept confirmation dialog pointer events

--- designer_v2/lib/features/forms/unsaved_changes_dialog.dart
+++ designer_v2/lib/features/forms/unsaved_changes_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pointer_interceptor/pointer_interceptor.dart';
 import 'package:studyu_designer_v2/common_views/confirmation_dialog.dart';
 import 'package:studyu_designer_v2/localization/app_translation.dart';
 
@@ -8,22 +7,20 @@ class UnsavedChangesDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PointerInterceptor(
-      child: StandardConfirmationDialog(
-        title: tr.dialog_unsaved_changes_title,
-        message: tr.dialog_unsaved_changes_description,
-        actions: [
-          ConfirmationDialogAction(
-            label: tr.dialog_action_unsaved_changes_stay,
-            onPressed: () => Navigator.pop(context, false),
-          ),
-          ConfirmationDialogAction(
-            label: tr.dialog_action_unsaved_changes_discard,
-            isDestructive: true,
-            onPressed: () => Navigator.pop(context, true),
-          ),
-        ],
-      ),
+    return StandardConfirmationDialog(
+      title: tr.dialog_unsaved_changes_title,
+      message: tr.dialog_unsaved_changes_description,
+      actions: [
+        ConfirmationDialogAction(
+          label: tr.dialog_action_unsaved_changes_stay,
+          onPressed: () => Navigator.pop(context, false),
+        ),
+        ConfirmationDialogAction(
+          label: tr.dialog_action_unsaved_changes_discard,
+          isDestructive: true,
+          onPressed: () => Navigator.pop(context, true),
+        ),
+      ],
     );
   }
 }
